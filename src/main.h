31d97fc2d5d0ee0992262a01af2331407641c4f6
Peter Todd <pete@petertodd.org>
2013-10-24 04:52:16 -0400

    Show short scriptPubKeys correctly
    Previously bitcoin-qt's -debug transaction info was showing CTxOut([error])
    
    It is valid for a scriptPubKey to be any size, for example simply
    OP_RETURN is valid and can be used to destroy a TXOUT to mining fees.
    
    Backport bitcoin commit 005609539b2184a474ffe9ebfe883984c900a3fb
    Backported by: Sunny King <sunnyking9999@gmail.com>
    (cherry picked from commit 005609539b2184a474ffe9ebfe883984c900a3fb)
    
    Conflicts:
    
    	src/core.cpp

--- src/main.h
+++ src/main.h
@@ -400,8 +400,6 @@ public:
     std::string ToString() const
     {
         if (IsEmpty()) return "CTxOut(empty)";
-        if (scriptPubKey.size() < 6)
-            return "CTxOut(error)";
         return strprintf("CTxOut(nValue=%s, scriptPubKey=%s)", FormatMoney(nValue).c_str(), scriptPubKey.ToString().c_str());
     }
 
