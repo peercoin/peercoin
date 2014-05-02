c4b38a3ebe14c50c2b25eef60cabdffa038c838b
bitpar <binarypar@gmail.com>
2014-05-02 07:42:27 -0400

    Build instructions for OS X 10.8 and 10.9 with MacPorts and Homebrew

--- src/qt/macdockiconhandler.h
+++ src/qt/macdockiconhandler.h
@@ -1,24 +1,37 @@
+// Copyright (c) 2011-2013 The Bitcoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
 #ifndef MACDOCKICONHANDLER_H
 #define MACDOCKICONHANDLER_H
 
-#include <QtCore/QObject>
+#include <QMainWindow>
+#include <QObject>
 
-class QMenu;
+QT_BEGIN_NAMESPACE
 class QIcon;
+class QMenu;
 class QWidget;
-class objc_object;
+QT_END_NAMESPACE
+
+#ifdef __OBJC__
+@class DockIconClickEventHandler;
+#else
+class DockIconClickEventHandler;
+#endif
 
 /** Macintosh-specific dock icon handler.
  */
 class MacDockIconHandler : public QObject
 {
     Q_OBJECT
+
 public:
     ~MacDockIconHandler();
 
     QMenu *dockMenu();
     void setIcon(const QIcon &icon);
-
+    void setMainWindow(QMainWindow *window);
     static MacDockIconHandler *instance();
 
     void handleDockIconClickEvent();
@@ -26,14 +39,13 @@ public:
 signals:
     void dockIconClicked();
 
-public slots:
-
 private:
     MacDockIconHandler();
 
-    objc_object *m_dockIconClickEventHandler;
+    DockIconClickEventHandler *m_dockIconClickEventHandler;
     QWidget *m_dummyWidget;
     QMenu *m_dockMenu;
+    QMainWindow *mainWindow;
 };
 
 #endif // MACDOCKICONCLICKHANDLER_H
--- src/serialize.h
+++ src/serialize.h
@@ -814,18 +814,6 @@ public:
     iterator insert(iterator it, const char& x=char()) { return vch.insert(it, x); }
     void insert(iterator it, size_type n, const char& x) { vch.insert(it, n, x); }
 
-    void insert(iterator it, const_iterator first, const_iterator last)
-    {
-        if (it == vch.begin() + nReadPos && last - first <= nReadPos)
-        {
-            // special case for inserting at the front when there's room
-            nReadPos -= (last - first);
-            memcpy(&vch[nReadPos], &first[0], last - first);
-        }
-        else
-            vch.insert(it, first, last);
-    }
-
     void insert(iterator it, std::vector<char>::const_iterator first, std::vector<char>::const_iterator last)
     {
         if (it == vch.begin() + nReadPos && last - first <= nReadPos)
