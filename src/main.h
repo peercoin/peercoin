cb82db0314476b253a3b9219d0d980cbbb8aef6a
Giannis Dzegoutanis <erasmospunk@gmail.com>
2014-12-05 16:10:11 +0100

    fix issue where getrawtransaction RPC call does not work for transactions in an orphaned block (cherry picked from commit 7ac8dc5d6177af4a35cc08a9ebdd5b5d34393d61)

--- src/main.h
+++ src/main.h
@@ -710,6 +710,7 @@ public:
     }
 
 
+    bool ReadFromDisk(CTxDB& txdb, const uint256& hash, CTxIndex& txindexRet);
     bool ReadFromDisk(CTxDB& txdb, COutPoint prevout, CTxIndex& txindexRet);
     bool ReadFromDisk(CTxDB& txdb, COutPoint prevout);
     bool ReadFromDisk(COutPoint prevout);
