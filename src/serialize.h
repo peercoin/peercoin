298f50c51ca883d2d26d720fb404c6e41690d136
Pieter Wuille <pieter.wuille@gmail.com>
2017-07-07 16:06:56 -0700

    Support serialization as another type without casting
    This adds a READWRITEAS(type, obj) macro which serializes obj as if it
    were casted to (const type&) when const, and to (type&) when non-const.
    
    This makes it usable in serialization code that uses a single
    implementation for both serialization and deserializing, which doesn't
    know the constness of the object involved.

--- src/serialize.h
+++ src/serialize.h
@@ -157,7 +157,12 @@ enum
     SER_POSMARKER       = (1 << 18),  // peercoin: for sending block headers with PoS marker, to allow headers-first syncronization
 };
 
-#define READWRITE(...)      (::SerReadWriteMany(s, ser_action, __VA_ARGS__))
+//! Convert the reference base type to X, without changing constness or reference type.
+template<typename X> X& ReadWriteAsHelper(X& x) { return x; }
+template<typename X> const X& ReadWriteAsHelper(const X& x) { return x; }
+
+#define READWRITE(...) (::SerReadWriteMany(s, ser_action, __VA_ARGS__))
+#define READWRITEAS(type, obj) (::SerReadWriteMany(s, ser_action, ReadWriteAsHelper<type>(obj)))
 
 /** 
  * Implement three methods for serializable objects. These are actually wrappers over
