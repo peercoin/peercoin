56b9865cbb0caf9dbe9a0880ec8b2cbc25c4fff0
hrobeers <hansrobeers@gmail.com>
2017-10-19 08:56:21 +0200

    Tag subversion with "rc2"

--- src/util.cpp
+++ src/util.cpp
@@ -1490,6 +1490,7 @@ std::string FormatSubVersion(const std::string& name, int nClientVersion, const 
     ss << "/";
     ss << "Peercoin:" << FormatVersion(PEERCOIN_VERSION);
     ss << "(" << CLIENT_BUILD << ")/";
+    ss << "rc2/";
     return ss.str();
 }
 
