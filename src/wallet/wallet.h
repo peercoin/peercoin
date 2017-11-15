6f5b6a75cd8633801c6e1922e4f413cc984014d3
Russell Yanofsky <russ@yanofsky.org>
2017-11-15 15:44:36 -0500

    Create new wallet databases as directories rather than files
    This change should make it easier for users to make complete backups of wallets
    because they can now just back up the specified `-wallet=<path>` path directly,
    instead of having to back up the specified path as well as the transaction log
    directory (for incompletely flushed wallets).
    
    Another advantage of this change is that if two wallets are located in the same
    directory, they will now use their own BerkeleyDB environments instead using a
    shared environment. Using a shared environment makes it difficult to manage and
    back up wallets separately because transaction log files will contain a mix of
    data from all wallets in the environment.

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -55,8 +55,6 @@ static const unsigned int DEFAULT_TX_CONFIRM_TARGET = 6;
 static const bool DEFAULT_WALLETBROADCAST = true;
 static const bool DEFAULT_DISABLE_WALLET = false;
 
-extern const char * DEFAULT_WALLET_DAT;
-
 static const int64_t TIMESTAMP_MIN = 0;
 
 class CBlockIndex;
