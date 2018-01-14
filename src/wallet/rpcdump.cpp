65403cf256f865af03797c08ab1559e35fd8f4d8
Alin Rus <alin@fsck.ro>
2018-01-14 18:15:31 +0100

    Remove useless string initialization.

--- src/wallet/rpcdump.cpp
+++ src/wallet/rpcdump.cpp
@@ -276,7 +276,7 @@ UniValue importaddress(const JSONRPCRequest& request)
         );
 
 
-    std::string strLabel = "";
+    std::string strLabel;
     if (!request.params[1].isNull())
         strLabel = request.params[1].get_str();
 
@@ -347,7 +347,7 @@ UniValue importpubkey(const JSONRPCRequest& request)
         );
 
 
-    std::string strLabel = "";
+    std::string strLabel;
     if (!request.params[1].isNull())
         strLabel = request.params[1].get_str();
 
