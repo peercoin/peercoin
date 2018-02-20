8b0c96a77263bb89ebc3401b2215897460a1b8cf
Jonas Schnelli <dev@jonasschnelli.ch>
2018-02-20 21:03:50 +1100

    Bugfix: respect user defined configuration file (-conf) when open conf. file from QT settings

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -413,7 +413,7 @@ void openDebugLogfile()
 
 bool openBitcoinConf()
 {
-    boost::filesystem::path pathConfig = GetConfigFile(BITCOIN_CONF_FILENAME);
+    boost::filesystem::path pathConfig = GetConfigFile(gArgs.GetArg("-conf", BITCOIN_CONF_FILENAME));
 
     /* Create the file */
     boost::filesystem::ofstream configFile(pathConfig, std::ios_base::app);
