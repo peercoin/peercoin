fabfad6489b1b1460067270a8d8581ba4781582a
Roman Zeyde <me@romanzey.de>
2018-03-22 13:55:22 +0200

    Make CTxMemPool::isSpent() const

--- src/txmempool.cpp
+++ src/txmempool.cpp
@@ -340,7 +340,7 @@ CTxMemPool::CTxMemPool() :
     nCheckFrequency = 0;
 }
 
-bool CTxMemPool::isSpent(const COutPoint& outpoint)
+bool CTxMemPool::isSpent(const COutPoint& outpoint) const
 {
     LOCK(cs);
     return mapNextTx.count(outpoint);
