c79fad9f9f89fc63c161fd1a1ba5d2dd2b815bf5
John Newbery <john@johnnewbery.com>
2018-06-27 11:53:08 -0400

    [wallet] factor out GetAvailableWatchOnlyBalance()

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -2030,11 +2030,6 @@ CAmount CWalletTx::GetImmatureWatchOnlyCredit(const bool fUseCache) const
     return 0;
 }
 
-CAmount CWalletTx::GetAvailableWatchOnlyCredit(const bool fUseCache) const
-{
-    return GetAvailableCredit(fUseCache, ISMINE_WATCH_ONLY);
-}
-
 CAmount CWalletTx::GetChange() const
 {
     if (fChangeCached)
@@ -2217,7 +2212,7 @@ CAmount CWallet::GetUnconfirmedWatchOnlyBalance() const
         {
             const CWalletTx* pcoin = &entry.second;
             if (!pcoin->IsTrusted() && pcoin->GetDepthInMainChain() == 0 && pcoin->InMempool())
-                nTotal += pcoin->GetAvailableWatchOnlyCredit();
+                nTotal += pcoin->GetAvailableCredit(true, ISMINE_WATCH_ONLY);
         }
     }
     return nTotal;
