dc3f7570d962e4d82e072623d2e89acd1c5eb659
Andrew Chow <achow101-github@achow101.com>
2018-07-03 17:18:52 -0700

    Make SignatureData able to store signatures and scripts
    In addition to having the scriptSig and scriptWitness, have SignatureData
    also be able to store just the signatures (pubkeys mapped to sigs) and
    scripts (script ids mapped to scripts).
    
    Also have DataFromTransaction be able to extract signatures and scripts
    from the scriptSig and scriptWitness of an input to put them in SignatureData.
    
    Adds a new SignatureChecker which takes a SignatureData and puts pubkeys
    and signatures into it when it successfully verifies a signature.
    
    Adds a new field in SignatureData which stores whether the SignatureData
    was complete. This allows us to also update the scriptSig and
    scriptWitness to the final one when updating a SignatureData with another
    one.

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -735,7 +735,7 @@ static UniValue combinerawtransaction(const JSONRPCRequest& request)
         // ... and merge in other signatures:
         for (const CMutableTransaction& txv : txVariants) {
             if (txv.vin.size() > i) {
-                sigdata = CombineSignatures(prevPubKey, TransactionSignatureChecker(&txConst, i, amount), sigdata, DataFromTransaction(txv, i));
+                sigdata = CombineSignatures(prevPubKey, TransactionSignatureChecker(&txConst, i, amount), sigdata, DataFromTransaction(txv, i, coin.out));
             }
         }
 
@@ -866,12 +866,12 @@ UniValue SignTransaction(CMutableTransaction& mtx, const UniValue& prevTxsUnival
         const CScript& prevPubKey = coin.out.scriptPubKey;
         const CAmount& amount = coin.out.nValue;
 
-        SignatureData sigdata;
+        SignatureData sigdata = DataFromTransaction(mtx, i, coin.out);
         // Only sign SIGHASH_SINGLE if there's a corresponding output:
         if (!fHashSingle || (i < mtx.vout.size())) {
             ProduceSignature(*keystore, MutableTransactionSignatureCreator(&mtx, i, amount, nHashType), prevPubKey, sigdata);
         }
-        sigdata = CombineSignatures(prevPubKey, TransactionSignatureChecker(&txConst, i, amount), sigdata, DataFromTransaction(mtx, i));
+        sigdata = CombineSignatures(prevPubKey, TransactionSignatureChecker(&txConst, i, amount), sigdata, DataFromTransaction(mtx, i, coin.out));
 
         UpdateInput(txin, sigdata);
 
