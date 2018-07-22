3b866b97934bfcfb6a860c12d1823682c755a187
MarcoFalke <falke.marco@gmail.com>
2018-07-22 10:43:57 -0400

    doc: Adjust bitcoincore.org links

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -29,11 +29,13 @@
  *
  * \section intro_sec Introduction
  *
- * This is the developer documentation of the reference client for an experimental new digital currency called Peercoin (https://www.peercoin.net/),
+ * This is the developer documentation of the reference client for an experimental new digital currency called Peercoin,
  * which enables instant payments to anyone, anywhere in the world. Peercoin uses peer-to-peer technology to operate
  * with no central authority: managing transactions and issuing money are carried out collectively by the network.
  *
  * The software is a community-driven open source project, released under the MIT license.
+ *
+ * See https://github.com/peercoin/peercoin and https://www.peercoin.net/ for further information about the project.
  *
  * \section Navigation
  * Use the buttons <code>Namespaces</code>, <code>Classes</code> or <code>Files</code> at the top of the page to start navigating the code.
