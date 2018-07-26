55eca3b377fe00f40235b0c641a5fcca425063eb
DrahtBot <39886733+DrahtBot@users.noreply.github.com>
2018-07-26 18:36:45 -0400

    Update copyright headers to 2018

--- src/test/versionbits_tests.cpp
+++ src/test/versionbits_tests.cpp
@@ -1,13 +1,13 @@
-// Copyright (c) 2014-2016 The Bitcoin Core developers
+// Copyright (c) 2014-2018 The Bitcoin Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#include "chain.h"
-#include "versionbits.h"
-#include "test/test_bitcoin.h"
-#include "chainparams.h"
-#include "validation.h"
-#include "consensus/params.h"
+#include <chain.h>
+#include <versionbits.h>
+#include <test/test_bitcoin.h>
+#include <chainparams.h>
+#include <validation.h>
+#include <consensus/params.h>
 
 #include <boost/test/unit_test.hpp>
 
