d1486c13af5cc28a6e554e7f1e52f5febb24dc6b
Ben Woosley <ben.woosley@gmail.com>
2018-07-31 14:02:34 -0400

    Replace remaining 0 with nullptr in Qt code
    Also used type-appropriate enum values such as Qt::NoItemFlags in
    some cases.
    
    All cases identified via -Wzero-as-null-pointer-constant

--- src/qt/platformstyle.cpp
+++ src/qt/platformstyle.cpp
@@ -143,6 +143,6 @@ const PlatformStyle *PlatformStyle::instantiate(const QString &platformId)
                     platform_styles[x].useExtraSpacing);
         }
     }
-    return 0;
+    return nullptr;
 }
 
