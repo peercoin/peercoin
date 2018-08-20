244af28ca716f5fa34510259291064a4576ac523
Gregory Sanders <gsanders87@gmail.com>
2018-08-20 12:57:06 -0400

    RPCTypeCheck bip32derivs arg in walletcreatefunded

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -4727,7 +4727,8 @@ UniValue walletcreatefundedpsbt(const JSONRPCRequest& request)
         UniValue::VARR,
         UniValueType(), // ARR or OBJ, checked later
         UniValue::VNUM,
-        UniValue::VOBJ
+        UniValue::VOBJ,
+        UniValue::VBOOL
         }, true
     );
 
