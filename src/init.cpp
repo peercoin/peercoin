b857a616a1368e2c27d06b5cabbc932d918d9ac9
Arvid Norberg <arvid@blockstream.io>
2018-09-13 10:36:41 -0700

    convert C-style (void) parameter lists to C++ style ()

--- src/init.cpp
+++ src/init.cpp
@@ -629,7 +629,7 @@ static void ThreadImport(std::vector<fs::path> vImportFiles)
  *  Ensure that Peercoin is running in a usable environment with all
  *  necessary library support.
  */
-static bool InitSanityCheck(void)
+static bool InitSanityCheck()
 {
     if(!ECC_InitSanityCheck()) {
         InitError("Elliptic curve cryptography sanity check failure. Aborting.");
