a2a7b45f2b11d426f9f7229748bc1a56cd5a2544
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2018-09-15 18:02:14 +0300

    Don't use systray icon on inappropriate systems
    Prevent a user from losing access to the main window by minimizing it to
    the tray on some systems (e.g. GNOME 3.26+).

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -140,7 +140,9 @@ BitcoinGUI::BitcoinGUI(interfaces::Node& node, const PlatformStyle *_platformSty
     createToolBars();
 
     // Create system tray icon and notification
-    createTrayIcon(networkStyle);
+    if (QSystemTrayIcon::isSystemTrayAvailable()) {
+        createTrayIcon(networkStyle);
+    }
 
     // Create status bar
     statusBar();
@@ -636,6 +638,8 @@ void BitcoinGUI::setWalletActionsEnabled(bool enabled)
 
 void BitcoinGUI::createTrayIcon(const NetworkStyle *networkStyle)
 {
+    assert(QSystemTrayIcon::isSystemTrayAvailable());
+
 #ifndef Q_OS_MAC
     trayIcon = new QSystemTrayIcon(this);
     QString toolTip = tr("%1 client").arg(tr(PACKAGE_NAME)) + " " + networkStyle->getTitleAddText();
