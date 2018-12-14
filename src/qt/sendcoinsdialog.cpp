a5bcfe041f1ecccc8a0abb4bcc91295e656a1300
bpay <bpay@users.noreply.github.com>
2018-12-13 23:39:05 -0500

    gui: Ensure tx send error highlight is visible
    If sending to multiple recipients and one of the recipient fields is malformed,
    the highlighted field may not be visible due to being scrolled out of view,
    leading to a confusing lack of error feedback when clicking Send. To avoid this
    problem ensure the first field containing an error is scrolled into view
    when Send is clicked.

--- src/qt/sendcoinsdialog.cpp
+++ src/qt/sendcoinsdialog.cpp
@@ -225,8 +225,9 @@ void SendCoinsDialog::on_sendButton_clicked()
             {
                 recipients.append(entry->getValue());
             }
-            else
+            else if (valid)
             {
+                ui->scrollArea->ensureWidgetVisible(entry);
                 valid = false;
             }
         }
