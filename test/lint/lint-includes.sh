bf99c73dd660d4a57cc077ec26d0469cdbf083d4
backpacker69 <backpacker69@protonmail.com>
2019-02-26 16:56:26 +0000

    update linting of includes

--- test/lint/lint-includes.sh
+++ test/lint/lint-includes.sh
@@ -47,13 +47,21 @@ fi
 
 EXPECTED_BOOST_INCLUDES=(
     boost/algorithm/string.hpp
+    boost/algorithm/string/case_conv.hpp
     boost/algorithm/string/classification.hpp
+    boost/algorithm/string/join.hpp
+    boost/algorithm/string/predicate.hpp
     boost/algorithm/string/replace.hpp
     boost/algorithm/string/split.hpp
+    boost/assign/list_of.hpp
+    boost/bind.hpp
     boost/chrono/chrono.hpp
     boost/date_time/posix_time/posix_time.hpp
     boost/filesystem.hpp
+    boost/filesystem/detail/utf8_codecvt_facet.hpp
     boost/filesystem/fstream.hpp
+    boost/interprocess/sync/file_lock.hpp
+    boost/lexical_cast.hpp
     boost/multi_index/hashed_index.hpp
     boost/multi_index/ordered_index.hpp
     boost/multi_index/sequenced_index.hpp
@@ -61,7 +69,8 @@ EXPECTED_BOOST_INCLUDES=(
     boost/optional.hpp
     boost/preprocessor/cat.hpp
     boost/preprocessor/stringize.hpp
-    boost/signals2/connection.hpp
+    boost/program_options/detail/config_file.hpp
+    boost/scoped_array.hpp
     boost/signals2/last_value.hpp
     boost/signals2/signal.hpp
     boost/test/unit_test.hpp
