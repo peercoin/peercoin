fb26599abc0de823e49e16e2a72e06ecb3361849
backpacker69 <backpacker69@protonmail.com>
2019-02-26 16:45:07 +0000

    fix linting include guards for peercoin

--- test/lint/lint-include-guards.sh
+++ test/lint/lint-include-guards.sh
@@ -7,7 +7,7 @@
 # Check include guards.
 
 export LC_ALL=C
-HEADER_ID_PREFIX="BITCOIN_"
+HEADER_ID_PREFIX="PEERCOIN_"
 HEADER_ID_SUFFIX="_H"
 
 REGEXP_EXCLUDE_FILES_WITH_PREFIX="src/(crypto/ctaes/|leveldb/|secp256k1/|tinyformat.h|univalue/)"
@@ -17,7 +17,7 @@ for HEADER_FILE in $(git ls-files -- "*.h" | grep -vE "^${REGEXP_EXCLUDE_FILES_W
 do
     HEADER_ID_BASE=$(cut -f2- -d/ <<< "${HEADER_FILE}" | sed "s/\.h$//g" | tr / _ | tr "[:lower:]" "[:upper:]")
     HEADER_ID="${HEADER_ID_PREFIX}${HEADER_ID_BASE}${HEADER_ID_SUFFIX}"
-    if [[ $(grep -cE "^#(ifndef|define) ${HEADER_ID}" "${HEADER_FILE}") != 2 ]]; then
+    if [[ $(grep -cE "^#(ifndef|define) (${HEADER_ID}|BITCOIN_${HEADER_ID_BASE}${HEADER_ID_SUFFIX})" "${HEADER_FILE}") != 2 ]]; then
         echo "${HEADER_FILE} seems to be missing the expected include guard:"
         echo "  #ifndef ${HEADER_ID}"
         echo "  #define ${HEADER_ID}"
