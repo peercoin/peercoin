72ad1043abd54e5a4b359f8fabe63aa49ad4f031
Suhas Daftuar <sdaftuar@gmail.com>
2019-03-08 15:48:41 -0500

    Check that tx_relay is initialized before access

--- src/net.h
+++ src/net.h
@@ -861,7 +861,7 @@ public:
 
     void AddInventoryKnown(const CInv& inv)
     {
-        {
+        if (m_tx_relay != nullptr) {
             LOCK(m_tx_relay->cs_tx_inventory);
             m_tx_relay->filterInventoryKnown.insert(inv.hash);
         }
@@ -869,7 +869,7 @@ public:
 
     void PushInventory(const CInv& inv)
     {
-        if (inv.type == MSG_TX) {
+        if (inv.type == MSG_TX && m_tx_relay != nullptr) {
             LOCK(m_tx_relay->cs_tx_inventory);
             if (!m_tx_relay->filterInventoryKnown.contains(inv.hash)) {
                 m_tx_relay->setInventoryTxToSend.insert(inv.hash);
