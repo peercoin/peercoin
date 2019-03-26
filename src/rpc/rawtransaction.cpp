4413f8993c6607fb802ac3fa067993999cb34822
Torkel Rogstad <torkel@rogstad.io>
2019-03-26 17:39:39 +0100

    rpc: Fix help text for signtransactionwithXXX

--- src/rpc/rawtransaction.cpp
+++ src/rpc/rawtransaction.cpp
@@ -971,7 +971,7 @@ static UniValue signrawtransactionwithkey(const JSONRPCRequest& request)
                                     {"vout", RPCArg::Type::NUM, RPCArg::Optional::NO, "The output number"},
                                     {"scriptPubKey", RPCArg::Type::STR_HEX, RPCArg::Optional::NO, "script key"},
                                     {"redeemScript", RPCArg::Type::STR_HEX, RPCArg::Optional::OMITTED, "(required for P2SH or P2WSH) redeem script"},
-                                    {"amount", RPCArg::Type::AMOUNT, RPCArg::Optional::NO, "The amount spent"},
+                                    {"amount", RPCArg::Type::AMOUNT, RPCArg::Optional::OMITTED, "(required for Segwit inputs) the amount spent"},
                                 },
                                 },
                         },
