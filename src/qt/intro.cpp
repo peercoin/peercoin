6a4d1748eedf001c0de1e68b67432fe3fb4d776d
251 <13120787+251Labs@users.noreply.github.com>
2019-04-23 13:26:06 +0200

    Resolve the qt/guiutil <-> qt/optionsmodal CD
    This pull request attempts to resolve the `qt/guiutil` <-> `qt/optionsmodel`
    circular dependency.
    
    The circular dependency is resolved by moving the `Intro::getDefaultDataDirectory`
    member function to `GUIUtil::getDefaultDataDirectory`.

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -169,7 +169,7 @@ QString Intro::getDataDirectory()
 void Intro::setDataDirectory(const QString &dataDir)
 {
     ui->dataDirectory->setText(dataDir);
-    if(dataDir == getDefaultDataDirectory())
+    if(dataDir == GUIUtil::getDefaultDataDirectory())
     {
         ui->dataDirDefault->setChecked(true);
         ui->dataDirectory->setEnabled(false);
@@ -181,11 +181,6 @@ void Intro::setDataDirectory(const QString &dataDir)
     }
 }
 
-QString Intro::getDefaultDataDirectory()
-{
-    return GUIUtil::boostPathToQString(GetDefaultDataDir());
-}
-
 bool Intro::pickDataDirectory(interfaces::Node& node)
 {
     QSettings settings;
@@ -194,7 +189,7 @@ bool Intro::pickDataDirectory(interfaces::Node& node)
     if(!gArgs.GetArg("-datadir", "").empty())
         return true;
     /* 1) Default data directory for operating system */
-    QString dataDir = getDefaultDataDirectory();
+    QString dataDir = GUIUtil::getDefaultDataDirectory();
     /* 2) Allow QSettings to override default dir */
     dataDir = settings.value("strDataDir", dataDir).toString();
 
@@ -240,7 +235,7 @@ bool Intro::pickDataDirectory(interfaces::Node& node)
      * override -datadir in the bitcoin.conf file in the default data directory
      * (to be consistent with bitcoind behavior)
      */
-    if(dataDir != getDefaultDataDirectory()) {
+    if(dataDir != GUIUtil::getDefaultDataDirectory()) {
         node.softSetArg("-datadir", GUIUtil::qstringToBoostPath(dataDir).string()); // use OS locale for path setting
     }
     return true;
@@ -294,7 +289,7 @@ void Intro::on_ellipsisButton_clicked()
 
 void Intro::on_dataDirDefault_clicked()
 {
-    setDataDirectory(getDefaultDataDirectory());
+    setDataDirectory(GUIUtil::getDefaultDataDirectory());
 }
 
 void Intro::on_dataDirCustom_clicked()
