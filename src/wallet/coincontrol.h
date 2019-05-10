0d91071e506e6b2c251a5a742a186b22448ff7e4
Amiti Uttarwar <amiti@uttarwar.org>
2019-05-09 18:34:38 -0700

    extract min & max depth onto coin control

--- src/wallet/coincontrol.h
+++ src/wallet/coincontrol.h
@@ -10,6 +10,9 @@
 
 #include <boost/optional.hpp>
 
+const int DEFAULT_MIN_DEPTH = 0;
+const int DEFAULT_MAX_DEPTH = 9999999;
+
 /** Coin Control Features. */
 class CCoinControl
 {
@@ -29,7 +32,9 @@ public:
     //! Forbids inclusion of dirty (previously used) addresses
     bool m_avoid_address_reuse;
     //! Minimum chain depth value for coin availability
-    int m_min_depth{0};
+    int m_min_depth = DEFAULT_MIN_DEPTH;
+    //! Maximum chain depth value for coin availability
+    int m_max_depth = DEFAULT_MAX_DEPTH;
 
     CCoinControl()
     {
