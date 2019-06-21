ae7a7d323a79456caf4bf9b805be7497ecd83e45
Patrick Strateman <patrick.strateman@gmail.com>
2019-06-21 13:27:00 -0400

    rpc: Allow shutdown while in generateblocks
    By checking the shutdown flag every loop we can use the entire nonce space
    instead of breaking every 16 bits to check the shutdown flag.
    
    This has been possible since the shutdown flag was switched to an atomic,
    before that change it was controlled by a condition variable and lock.

--- src/rpc/mining.cpp
+++ src/rpc/mining.cpp
@@ -135,7 +135,6 @@ UniValue getnetworkghps(const JSONRPCRequest& request)
 
 static UniValue generateBlocks(const CScript& coinbase_script, int nGenerate, uint64_t nMaxTries, CWallet * const pwallet)
 {
-    static const int nInnerLoopCount = 0x10000;
     int nHeightEnd = 0;
     int nHeight = 0;
 
@@ -156,14 +155,14 @@ static UniValue generateBlocks(const CScript& coinbase_script, int nGenerate, ui
             LOCK(cs_main);
             IncrementExtraNonce(pblock, ::ChainActive().Tip(), nExtraNonce);
         }
-        while (nMaxTries > 0 && pblock->nNonce < nInnerLoopCount && !CheckProofOfWork(pblock->GetHash(), pblock->nBits, Params().GetConsensus())) {
+        while (nMaxTries > 0 && pblock->nNonce < std::numeric_limits<uint32_t>::max() && !CheckProofOfWork(pblock->GetHash(), pblock->nBits, Params().GetConsensus()) && !ShutdownRequested()) {
             ++pblock->nNonce;
             --nMaxTries;
         }
-        if (nMaxTries == 0) {
+        if (nMaxTries == 0 || ShutdownRequested()) {
             break;
         }
-        if (pblock->nNonce == nInnerLoopCount) {
+        if (pblock->nNonce == std::numeric_limits<uint32_t>::max()) {
             continue;
         }
 
