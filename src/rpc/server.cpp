9be57044f0c9540025a784ab12137c438704ba72
Jon Atack <jon@atack.com>
2019-07-27 16:28:48 +0200

    log: harmonize bitcoind server logging
    Harmonize the user-facing output of the `bitcoind -daemon`, `bitcoin-cli help stop`, `bitcoin-cli stop`, and `bitcoind -version` commands to be consistent with each other as well as with the "Bitcoin Core is probably already running" messages, e.g. `git grep 'probably already running.")'`.

--- src/rpc/server.cpp
+++ src/rpc/server.cpp
@@ -163,7 +163,7 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     if (jsonRequest.fHelp || jsonRequest.params.size() > 1)
         throw std::runtime_error(
             RPCHelpMan{"stop",
-                "\nStop Peercoin server.",
+                "\nRequest a graceful shutdown of " PACKAGE_NAME ".",
                 {},
                 RPCResults{},
                 RPCExamples{""},
@@ -174,7 +174,7 @@ UniValue stop(const JSONRPCRequest& jsonRequest)
     if (jsonRequest.params[0].isNum()) {
         MilliSleep(jsonRequest.params[0].get_int());
     }
-    return "Peercoin server stopping";
+    return PACKAGE_NAME " stopping";
 }
 
 static UniValue uptime(const JSONRPCRequest& jsonRequest)
