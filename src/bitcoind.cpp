b32dfd2fdb3dba591abf67b7156a4c0f09a563d2
Jon Layton <me@jonl.io>
2019-09-18 18:54:20 -0400

    doc: Move doxygen intro to file for USE_MDFILE_AS_MANPAGE
    doc: Change header to notitle

--- src/bitcoind.cpp
+++ src/bitcoind.cpp
@@ -25,24 +25,6 @@
 
 const std::function<std::string(const char*)> G_TRANSLATION_FUN = nullptr;
 
-/* Introduction text for doxygen: */
-
-/*! \mainpage Developer documentation
- *
- * \section intro_sec Introduction
- *
- * This is the developer documentation of the reference client for an experimental new digital currency called Peercoin,
- * which enables instant payments to anyone, anywhere in the world. Peercoin uses peer-to-peer technology to operate
- * with no central authority: managing transactions and issuing money are carried out collectively by the network.
- *
- * The software is a community-driven open source project, released under the MIT license.
- *
- * See https://github.com/peercoin/peercoin and https://www.peercoin.net/ for further information about the project.
- *
- * \section Navigation
- * Use the buttons <code>Namespaces</code>, <code>Classes</code> or <code>Files</code> at the top of the page to start navigating the code.
- */
-
 static void WaitForShutdown()
 {
     while (!ShutdownRequested())
