3902bb04cfda4b5a5322fc351d392a1c988ab1ce
MarcoFalke <falke.marco@gmail.com>
2019-09-23 14:45:51 -0400

    util: Add CHECK_NONFATAL and use it in src/rpc

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -10,11 +10,11 @@
 #include <chain.h>
 #include <chainparams.h>
 #include <coins.h>
-#include <node/coinstats.h>
 #include <consensus/validation.h>
 #include <core_io.h>
 #include <hash.h>
 #include <index/blockfilterindex.h>
+#include <node/coinstats.h>
 #include <policy/policy.h>
 #include <policy/rbf.h>
 #include <primitives/transaction.h>
@@ -33,7 +33,6 @@
 #include <validationinterface.h>
 #include <warnings.h>
 
-#include <assert.h>
 #include <stdint.h>
 
 #include <univalue.h>
