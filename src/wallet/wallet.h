8aa314aa8581bc9632111b3d75d12d3a995aaa00
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2019-10-20 21:00:33 +0200

    wallet: Remove unused GetLabelName

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1224,8 +1224,6 @@ public:
 
     bool DelAddressBook(const CTxDestination& address);
 
-    const std::string& GetLabelName(const CScript& scriptPubKey) const EXCLUSIVE_LOCKS_REQUIRED(cs_wallet);
-
     unsigned int GetKeyPoolSize() EXCLUSIVE_LOCKS_REQUIRED(cs_wallet)
     {
         AssertLockHeld(cs_wallet);
