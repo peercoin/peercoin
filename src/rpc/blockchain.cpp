192db1e9e56a7a58effb312a881e0e648fda635d
MarcoFalke <falke.marco@gmail.com>
2019-11-05 14:01:18 -0500

    rpc: Remove unused boost::this_thread::interruption_point

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -42,8 +42,6 @@
 #include <kernel.h>
 #include <validation.h>
 
-#include <boost/thread/thread.hpp> // boost::thread::interrupt
-
 #include <condition_variable>
 #include <memory>
 #include <mutex>
@@ -1841,7 +1839,6 @@ bool FindScriptPubKey(std::atomic<int>& scan_progress, const std::atomic<bool>& 
         Coin coin;
         if (!cursor->GetKey(key) || !cursor->GetValue(coin)) return false;
         if (++count % 8192 == 0) {
-            boost::this_thread::interruption_point();
             if (should_abort) {
                 // allow to abort the scan via the abort reference
                 return false;
