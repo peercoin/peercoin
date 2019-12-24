bfec21980f95df732465dca0ebb6e86dd9f8536a
Gleb Naumenko <naumenko.gs@gmail.com>
2019-12-24 13:18:44 -0500

    Integrate ASN bucketing in Addrman and add tests
    Instead of using /16 netgroups to bucket nodes in Addrman for connection
    diversification, ASN, which better represents an actor in terms
    of network-layer infrastructure, is used.
    For testing, asmap.raw is used. It represents a minimal
    asmap needed for testing purposes.

--- src/net.h
+++ src/net.h
@@ -157,6 +157,7 @@ public:
         bool m_use_addrman_outgoing = true;
         std::vector<std::string> m_specified_outgoing;
         std::vector<std::string> m_added_nodes;
+        std::vector<bool> m_asmap;
     };
 
     void Init(const Options& connOptions) {
@@ -333,6 +334,8 @@ public:
         Variable intervals will result in privacy decrease.
     */
     int64_t PoissonNextSendInbound(int64_t now, int average_interval_seconds);
+
+    void SetAsmap(std::vector<bool> asmap) { addrman.m_asmap = asmap; }
 
 private:
     struct ListenSocket {
