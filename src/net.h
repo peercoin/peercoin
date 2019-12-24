be2c478d33832d648e6e452cd50c07d73dccd4d3
Gleb Naumenko <naumenko.gs@gmail.com>
2019-12-24 13:26:46 -0500

    Return mapped AS in RPC call getpeerinfo
    If ASN bucketing is used, return a corresponding AS
    used in bucketing for a given peer.

--- src/net.h
+++ src/net.h
@@ -611,6 +611,7 @@ public:
     CAddress addr;
     // Bind address of our side of the connection
     CAddress addrBind;
+    uint32_t m_mapped_as;
 };
 
 
@@ -990,7 +991,7 @@ public:
 
     void CloseSocketDisconnect();
 
-    void copyStats(CNodeStats &stats);
+    void copyStats(CNodeStats &stats, std::vector<bool> &m_asmap);
 
     ServiceFlags GetLocalServices() const
     {
