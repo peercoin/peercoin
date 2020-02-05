d537a9ce9c47365b60c9766f6b673f0b01b4a0c4
Jeremy Rubin <j@rubin.io>
2020-02-05 11:57:32 -0800

    Add SHA256Uint256 helper functions

--- src/hash.cpp
+++ src/hash.cpp
@@ -78,6 +78,12 @@ void BIP32Hash(const ChainCode &chainCode, unsigned int nChild, unsigned char he
     CHMAC_SHA512(chainCode.begin(), chainCode.size()).Write(&header, 1).Write(data, 32).Write(num, 4).Finalize(output);
 }
 
+uint256 SHA256Uint256(const uint256& input)
+{
+    uint256 result;
+    CSHA256().Write(input.begin(), 32).Finalize(result.begin());
+    return result;
+}
 
 int32_t peercoinRandseed;
 int univHash(const uint256 &x) {
