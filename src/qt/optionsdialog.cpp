8060f018d9db1120bc54122a7b6fe4e47860e9ec
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-02-23 02:25:21 +0200

    gui: Add NAT-PMP network option

--- src/qt/optionsdialog.cpp
+++ src/qt/optionsdialog.cpp
@@ -46,6 +46,9 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
 #ifndef USE_UPNP
     ui->mapPortUpnp->setEnabled(false);
 #endif
+#ifndef USE_NATPMP
+    ui->mapPortNatpmp->setEnabled(false);
+#endif
     connect(this, &QDialog::accepted, [this](){
         QSettings settings;
         model->node().mapPort(settings.value("fUseUPnP").toBool(), settings.value("fUseNatpmp").toBool());
@@ -210,6 +213,7 @@ void OptionsDialog::setMapper()
 
     /* Network */
     mapper->addMapping(ui->mapPortUpnp, OptionsModel::MapPortUPnP);
+    mapper->addMapping(ui->mapPortNatpmp, OptionsModel::MapPortNatpmp);
     mapper->addMapping(ui->allowIncoming, OptionsModel::Listen);
 
     mapper->addMapping(ui->connectSocks, OptionsModel::ProxyUse);
