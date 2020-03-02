2ec3c233cbf524e93a5ff66917e03a626c192946
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2020-03-02 15:57:25 +0900

    make BlockUntilSyncedToCurrentChain() const
    The method checks the chain tip for the best block, and calls SyncWithValidationInterfaceQueue() (a standalone function) if necessary.

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -1156,7 +1156,7 @@ void CWallet::UpdatedBlockTip()
 }
 
 
-void CWallet::BlockUntilSyncedToCurrentChain() {
+void CWallet::BlockUntilSyncedToCurrentChain() const {
     AssertLockNotHeld(cs_wallet);
     // Skip the queue-draining stuff if we know we're caught up with
     // ::ChainActive().Tip(), otherwise put a callback in the validation interface queue and wait
