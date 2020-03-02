1a06f91e490d32fdc5662a53f8dc7af5f7bca19d
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2020-03-02 17:23:34 +0900

    wallet: make BackupWallet() const

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1074,7 +1074,7 @@ public:
      */
     void postInitProcess();
 
-    bool BackupWallet(const std::string& strDest);
+    bool BackupWallet(const std::string& strDest) const;
 
     /* Returns true if HD is enabled */
     bool IsHDEnabled() const;
