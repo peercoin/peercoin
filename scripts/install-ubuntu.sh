3f5a6ba97195dd2b86d7f8cb3bb4c26ecea00073
Chapman Shoop <chapman.shoop@gmail.com>
2020-03-26 14:20:53 -0700

    Configure to build tests, has been successfully running 0 tests :(

--- scripts/install-ubuntu.sh
+++ scripts/install-ubuntu.sh
@@ -8,6 +8,6 @@ g++ --version
 sudo ln -sf `pwd`/db4/include /usr/local/include/bdb4.8
 sudo ln -sf `pwd`/db4/lib/*.a /usr/local/lib
 ./autogen.sh
-./configure --with-gui=qt5
+./configure --with-gui=qt5 --enable-tests
 make -j$(nproc)
 make check
