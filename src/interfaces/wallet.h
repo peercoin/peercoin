986f1fee9a50a6390f3e15a89194cfb53faf98a4
Russell Yanofsky <russ@yanofsky.org>
2020-05-28 09:48:30 -0400

    refactor: Move wallet methods out of chain.h and node.h
    Add WalletClient interface so node interface is cleaner and don't need
    wallet-specific methods.
    
    The new NodeContext::wallet_client pointer will also be needed to eliminate
    global wallet variables like ::vpwallets, because createWallet(), loadWallet(),
    getWallets(), etc methods called by the GUI need a way to get a reference to
    the list of open wallets if it is no longer a global variable.
    
    Also tweaks splash screen registration for load wallet events to be delayed
    until after wallet client is created.

--- src/interfaces/wallet.h
+++ src/interfaces/wallet.h
@@ -29,6 +29,7 @@ enum class FeeReason;
 enum class OutputType;
 enum class TransactionError;
 struct PartiallySignedTransaction;
+struct WalletContext;
 struct bilingual_str;
 namespace wallet {
 class CCoinControl;
@@ -327,6 +328,34 @@ public:
    virtual wallet::WalletContext* context() { return nullptr; }
 };
 
+//! Wallet chain client that in addition to having chain client methods for
+//! starting up, shutting down, and registering RPCs, also has additional
+//! methods (called by the GUI) to load and create wallets.
+class WalletClient : public ChainClient
+{
+public:
+    //! Create new wallet.
+    virtual std::unique_ptr<Wallet> createWallet(const std::string& name, const SecureString& passphrase, uint64_t wallet_creation_flags, WalletCreationStatus& status, bilingual_str& error, std::vector<bilingual_str>& warnings) = 0;
+
+   //! Load existing wallet.
+   virtual std::unique_ptr<Wallet> loadWallet(const std::string& name, bilingual_str& error, std::vector<bilingual_str>& warnings) = 0;
+
+   //! Return default wallet directory.
+   virtual std::string getWalletDir() = 0;
+
+   //! Return available wallets in wallet directory.
+   virtual std::vector<std::string> listWalletDir() = 0;
+
+   //! Return interfaces for accessing wallets (if any).
+   virtual std::vector<std::unique_ptr<Wallet>> getWallets() = 0;
+
+   //! Register handler for load wallet messages. This callback is triggered by
+   //! createWallet and loadWallet above, and also triggered when wallets are
+   //! loaded at startup or by RPC.
+   using LoadWalletFn = std::function<void(std::unique_ptr<Wallet> wallet)>;
+   virtual std::unique_ptr<Handler> handleLoadWallet(LoadWalletFn fn) = 0;
+};
+
 //! Information about one wallet address.
 struct WalletAddress
 {
@@ -410,6 +439,10 @@ std::unique_ptr<Wallet> MakeWallet(wallet::WalletContext& context, const std::sh
 //! Return implementation of ChainClient interface for a wallet loader. This
 //! function will be undefined in builds where ENABLE_WALLET is false.
 std::unique_ptr<WalletLoader> MakeWalletLoader(Chain& chain, ArgsManager& args);
+
+//! Return implementation of ChainClient interface for a wallet client. This
+//! function will be undefined in builds where ENABLE_WALLET is false.
+std::unique_ptr<WalletClient> MakeWalletClient(Chain& chain, ArgsManager& args, std::vector<std::string> wallet_filenames);
 
 } // namespace interfaces
 
