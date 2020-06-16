80a4da32acdb8ab748c75f3ad54c3b4a12cbf6a9
MarcoFalke <falke.marco@gmail.com>
2020-06-16 11:02:46 -0400

    refactor: Use AbortError in FatalError
    This is needed for consistency with AbortNode

--- src/ui_interface.h
+++ src/ui_interface.h
@@ -123,6 +123,7 @@ void InitWarning(const bilingual_str& str);
 
 /** Show error message **/
 bool InitError(const bilingual_str& str);
+constexpr auto AbortError = InitError;
 
 extern CClientUIInterface uiInterface;
 
