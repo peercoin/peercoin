35e3c71ed0b30ca483e16e7d66cc9457c32c8c54
Ivan Metlushko <metlushko@gmail.com>
2020-06-24 09:08:41 +0700

    test: use pgrep for better compatibility
    pidof is not available on BSD system, while pgrep is present on BSD, Linux and macOS

--- test/functional/test_runner.py
+++ test/functional/test_runner.py
@@ -394,11 +394,12 @@ def run_tests(*, test_list, src_dir, build_dir, tmpdir, jobs=1, enable_coverage=
     args = args or []
 
     # Warn if peercoind is already running
-    # pidof might fail or return an empty string if bitcoind is not running
     try:
-        if subprocess.check_output(["pidof", "peercoind"]) not in [b'']:
+        # pgrep exits with code zero when one or more matching processes found
+        if subprocess.run(["pgrep", "-x", "peercoind"], stdout=subprocess.DEVNULL).returncode == 0:
             print("%sWARNING!%s There is already a peercoind process running on this system. Tests may fail unexpectedly due to resource contention!" % (BOLD[1], BOLD[0]))
-    except (OSError, subprocess.SubprocessError):
+    except OSError:
+        # pgrep not supported
         pass
 
     # Warn if there is a cache directory
