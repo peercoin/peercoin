equalishcoin/equalishcoind

rpc: improve getaddressinfo RPCHelpman content

src/wallet/rpcwallet.cpp
@@ -951,7 +951,7 @@ static UniValue addmultisigaddress(const JSONRPCRequest& request)
     }
 
             RPCHelpMan{"addmultisigaddress",
-                "\nAdd a nrequired-to-sign multisignature address to the wallet. Requires a new wallet backup.\n"
+                "\nAdd an nrequired-to-sign multisignature address to the wallet. Requires a new wallet backup.\n"
                 "Each key is a Bitcoin address or hex-encoded public key.\n"
                 "This functionality is only intended for use with non-watchonly addresses.\n"
                 "See `importaddress` for watchonly p2sh address support.\n"
@@ -3710,48 +3710,48 @@ UniValue getaddressinfo(const JSONRPCRequest& request)
 
             RPCHelpMan{"getaddressinfo",
                 "\nReturn information about the given bitcoin address.\n"
-                "Some information requires the address to be in the wallet.\n",
+                "Some of the information will only be present if the address is in the active wallet.\n",
                 {
-                    {"address", RPCArg::Type::STR, RPCArg::Optional::NO, "The bitcoin address to get the information of."},
+                    {"address", RPCArg::Type::STR, RPCArg::Optional::NO, "The bitcoin address for which to get information."},
                 },
                 RPCResult{
             "{\n"
-            "  \"address\" : \"address\",              (string) The bitcoin address validated\n"
-            "  \"scriptPubKey\" : \"hex\",             (string) The hex-encoded scriptPubKey generated by the address\n"
-            "  \"ismine\" : true|false,              (boolean) If the address is yours or not\n"
-            "  \"iswatchonly\" : true|false,         (boolean) If the address is watchonly\n"
-            "  \"solvable\" : true|false,            (boolean) Whether we know how to spend coins sent to this address, ignoring the possible lack of private keys\n"
-            "  \"desc\" : \"desc\",                    (string, optional) A descriptor for spending coins sent to this address (only when solvable)\n"
-            "  \"isscript\" : true|false,            (boolean) If the key is a script\n"
-            "  \"ischange\" : true|false,            (boolean) If the address was used for change output\n"
-            "  \"iswitness\" : true|false,           (boolean) If the address is a witness address\n"
-            "  \"witness_version\" : version         (numeric, optional) The version number of the witness program\n"
-            "  \"witness_program\" : \"hex\"           (string, optional) The hex value of the witness program\n"
-            "  \"script\" : \"type\"                   (string, optional) The output script type. Only if \"isscript\" is true and the redeemscript is known. Possible\n"
+            "  \"address\" : \"address\",              (string) The bitcoin address validated.\n"
+            "  \"scriptPubKey\" : \"hex\",             (string) The hex-encoded scriptPubKey generated by the address.\n"
+            "  \"ismine\" : true|false,              (boolean) If the address is yours.\n"
+            "  \"iswatchonly\" : true|false,         (boolean) If the address is watchonly.\n"
+            "  \"solvable\" : true|false,            (boolean) If we know how to spend coins sent to this address, ignoring the possible lack of private keys.\n"
+            "  \"desc\" : \"desc\",                    (string, optional) A descriptor for spending coins sent to this address (only when solvable).\n"
+            "  \"isscript\" : true|false,            (boolean) If the key is a script.\n"
+            "  \"ischange\" : true|false,            (boolean) If the address was used for change output.\n"
+            "  \"iswitness\" : true|false,           (boolean) If the address is a witness address.\n"
+            "  \"witness_version\" : version         (numeric, optional) The version number of the witness program.\n"
+            "  \"witness_program\" : \"hex\"           (string, optional) The hex value of the witness program.\n"
+            "  \"script\" : \"type\"                   (string, optional) The output script type. Only if isscript is true and the redeemscript is known. Possible\n"
             "                                                         types: nonstandard, pubkey, pubkeyhash, scripthash, multisig, nulldata, witness_v0_keyhash,\n"
-            "                                                         witness_v0_scripthash, witness_unknown\n"
-            "  \"hex\" : \"hex\",                      (string, optional) The redeemscript for the p2sh address\n"
-            "  \"pubkeys\"                           (string, optional) Array of pubkeys associated with the known redeemscript (only if \"script\" is \"multisig\")\n"
+            "                                                         witness_v0_scripthash, witness_unknown.\n"
+            "  \"hex\" : \"hex\",                      (string, optional) The redeemscript for the p2sh address.\n"
+            "  \"pubkeys\"                           (array, optional) Array of pubkeys associated with the known redeemscript (only if script is multisig).\n"
             "    [\n"
-            "      \"pubkey\"\n"
+            "      \"pubkey\" (string)\n"
             "      ,...\n"
             "    ]\n"
-            "  \"sigsrequired\" : xxxxx              (numeric, optional) Number of signatures required to spend multisig output (only if \"script\" is \"multisig\")\n"
-            "  \"pubkey\" : \"publickeyhex\",          (string, optional) The hex value of the raw public key, for single-key addresses (possibly embedded in P2SH or P2WSH)\n"
-            "  \"embedded\" : {...},                 (object, optional) Information about the address embedded in P2SH or P2WSH, if relevant and known. It includes all\n"
-            "                                                         getaddressinfo output fields for the embedded address, excluding metadata (\"timestamp\", \"hdkeypath\",\n"
-            "                                                         \"hdseedid\") and relation to the wallet (\"ismine\", \"iswatchonly\").\n"
-            "  \"iscompressed\" : true|false,        (boolean, optional) If the pubkey is compressed\n"
-            "  \"label\" :  \"label\"                  (string) The label associated with the address, \"\" is the default label\n"
-            "  \"timestamp\" : timestamp,            (number, optional) The creation time of the key if available in seconds since epoch (Jan 1 1970 GMT)\n"
-            "  \"hdkeypath\" : \"keypath\"             (string, optional) The HD keypath if the key is HD and available\n"
-            "  \"hdseedid\" : \"<hash160>\"            (string, optional) The Hash160 of the HD seed\n"
-            "  \"hdmasterfingerprint\" : \"<hash160>\" (string, optional) The fingperint of the master key.\n"
+            "  \"sigsrequired\" : xxxxx              (numeric, optional) The number of signatures required to spend multisig output (only if script is multisig).\n"
+            "  \"pubkey\" : \"publickeyhex\",          (string, optional) The hex value of the raw public key for single-key addresses (possibly embedded in P2SH or P2WSH).\n"
+            "  \"embedded\" : {...},                 (object, optional) Information about the address embedded in P2SH or P2WSH, if relevant and known. Includes all\n"
+            "                                                         getaddressinfo output fields for the embedded address, excluding metadata (timestamp, hdkeypath,\n"
+            "                                                         hdseedid) and relation to the wallet (ismine, iswatchonly).\n"
+            "  \"iscompressed\" : true|false,        (boolean, optional) If the pubkey is compressed.\n"
+            "  \"label\" :  \"label\"                  (string) The label associated with the address. Defaults to \"\".\n"
+            "  \"timestamp\" : timestamp,            (number, optional) The creation time of the key if available, expressed in seconds since Epoch Time (Jan 1 1970 GMT).\n"
+            "  \"hdkeypath\" : \"keypath\"             (string, optional) The HD keypath, if the key is HD and available.\n"
+            "  \"hdseedid\" : \"<hash160>\"            (string, optional) The Hash160 of the HD seed.\n"
+            "  \"hdmasterfingerprint\" : \"<hash160>\" (string, optional) The fingerprint of the master key.\n"
             "  \"labels\"                            (object) Array of labels associated with the address.\n"
             "    [\n"
             "      { (json object of label data)\n"
-            "        \"name\": \"labelname\" (string) The label\n"
-            "        \"purpose\": \"string\" (string) Purpose of address (\"send\" for sending address, \"receive\" for receiving address)\n"
+            "        \"name\": \"label name\" (string) The label.\n"
+            "        \"purpose\": \"purpose\" (string) The purpose of the associated address (send or receive).\n"
             "      },...\n"
             "    ]\n"
             "}\n"

peercoin/peercoin

src/wallet/rpcwallet.cpp
@@ -924,7 +924,7 @@ static UniValue addmultisigaddress(const JSONRPCRequest& request)
     }
 
             RPCHelpMan{"addmultisigaddress",
-                "\nAdd a nrequired-to-sign multisignature address to the wallet. Requires a new wallet backup.\n"
+                "\nAdd an nrequired-to-sign multisignature address to the wallet. Requires a new wallet backup.\n"
                 "Each key is a peercoin address or hex-encoded public key.\n"
                 "This functionality is only intended for use with non-watchonly addresses.\n"
                 "See `importaddress` for watchonly p2sh address support.\n"
@@ -3688,48 +3688,48 @@ UniValue getaddressinfo(const JSONRPCRequest& request)
 
             RPCHelpMan{"getaddressinfo",
                 "\nReturn information about the given peercoin address.\n"
-                "Some information requires the address to be in the wallet.\n",
+                "Some of the information will only be present if the address is in the active wallet.\n",
                 {
-                    {"address", RPCArg::Type::STR, RPCArg::Optional::NO, "The peercoin address to get the information of."},
+                    {"address", RPCArg::Type::STR, RPCArg::Optional::NO, "The peercoin address for which to get information."},
                 },
                 RPCResult{
             "{\n"
-            "  \"address\" : \"address\",              (string) The peercoin address validated\n"
-            "  \"scriptPubKey\" : \"hex\",             (string) The hex-encoded scriptPubKey generated by the address\n"
-            "  \"ismine\" : true|false,              (boolean) If the address is yours or not\n"
-            "  \"iswatchonly\" : true|false,         (boolean) If the address is watchonly\n"
-            "  \"solvable\" : true|false,            (boolean) Whether we know how to spend coins sent to this address, ignoring the possible lack of private keys\n"
-            "  \"desc\" : \"desc\",                    (string, optional) A descriptor for spending coins sent to this address (only when solvable)\n"
-            "  \"isscript\" : true|false,            (boolean) If the key is a script\n"
-            "  \"ischange\" : true|false,            (boolean) If the address was used for change output\n"
-            "  \"iswitness\" : true|false,           (boolean) If the address is a witness address\n"
-            "  \"witness_version\" : version         (numeric, optional) The version number of the witness program\n"
-            "  \"witness_program\" : \"hex\"           (string, optional) The hex value of the witness program\n"
-            "  \"script\" : \"type\"                   (string, optional) The output script type. Only if \"isscript\" is true and the redeemscript is known. Possible\n"
+            "  \"address\" : \"address\",              (string) The peercoin address validated.\n"
+            "  \"scriptPubKey\" : \"hex\",             (string) The hex-encoded scriptPubKey generated by the address.\n"
+            "  \"ismine\" : true|false,              (boolean) If the address is yours.\n"
+            "  \"iswatchonly\" : true|false,         (boolean) If the address is watchonly.\n"
+            "  \"solvable\" : true|false,            (boolean) If we know how to spend coins sent to this address, ignoring the possible lack of private keys.\n"
+            "  \"desc\" : \"desc\",                    (string, optional) A descriptor for spending coins sent to this address (only when solvable).\n"
+            "  \"isscript\" : true|false,            (boolean) If the key is a script.\n"
+            "  \"ischange\" : true|false,            (boolean) If the address was used for change output.\n"
+            "  \"iswitness\" : true|false,           (boolean) If the address is a witness address.\n"
+            "  \"witness_version\" : version         (numeric, optional) The version number of the witness program.\n"
+            "  \"witness_program\" : \"hex\"           (string, optional) The hex value of the witness program.\n"
+            "  \"script\" : \"type\"                   (string, optional) The output script type. Only if isscript is true and the redeemscript is known. Possible\n"
             "                                                         types: nonstandard, pubkey, pubkeyhash, scripthash, multisig, nulldata, witness_v0_keyhash,\n"
-            "                                                         witness_v0_scripthash, witness_unknown\n"
-            "  \"hex\" : \"hex\",                      (string, optional) The redeemscript for the p2sh address\n"
-            "  \"pubkeys\"                           (string, optional) Array of pubkeys associated with the known redeemscript (only if \"script\" is \"multisig\")\n"
+            "                                                         witness_v0_scripthash, witness_unknown.\n"
+            "  \"hex\" : \"hex\",                      (string, optional) The redeemscript for the p2sh address.\n"
+            "  \"pubkeys\"                           (array, optional) Array of pubkeys associated with the known redeemscript (only if script is multisig).\n"
             "    [\n"
-            "      \"pubkey\"\n"
+            "      \"pubkey\" (string)\n"
             "      ,...\n"
             "    ]\n"
-            "  \"sigsrequired\" : xxxxx              (numeric, optional) Number of signatures required to spend multisig output (only if \"script\" is \"multisig\")\n"
-            "  \"pubkey\" : \"publickeyhex\",          (string, optional) The hex value of the raw public key, for single-key addresses (possibly embedded in P2SH or P2WSH)\n"
-            "  \"embedded\" : {...},                 (object, optional) Information about the address embedded in P2SH or P2WSH, if relevant and known. It includes all\n"
-            "                                                         getaddressinfo output fields for the embedded address, excluding metadata (\"timestamp\", \"hdkeypath\",\n"
-            "                                                         \"hdseedid\") and relation to the wallet (\"ismine\", \"iswatchonly\").\n"
-            "  \"iscompressed\" : true|false,        (boolean, optional) If the pubkey is compressed\n"
-            "  \"label\" :  \"label\"                  (string) The label associated with the address, \"\" is the default label\n"
-            "  \"timestamp\" : timestamp,            (number, optional) The creation time of the key if available in seconds since epoch (Jan 1 1970 GMT)\n"
-            "  \"hdkeypath\" : \"keypath\"             (string, optional) The HD keypath if the key is HD and available\n"
-            "  \"hdseedid\" : \"<hash160>\"            (string, optional) The Hash160 of the HD seed\n"
-            "  \"hdmasterfingerprint\" : \"<hash160>\" (string, optional) The fingperint of the master key.\n"
+            "  \"sigsrequired\" : xxxxx              (numeric, optional) The number of signatures required to spend multisig output (only if script is multisig).\n"
+            "  \"pubkey\" : \"publickeyhex\",          (string, optional) The hex value of the raw public key for single-key addresses (possibly embedded in P2SH or P2WSH).\n"
+            "  \"embedded\" : {...},                 (object, optional) Information about the address embedded in P2SH or P2WSH, if relevant and known. Includes all\n"
+            "                                                         getaddressinfo output fields for the embedded address, excluding metadata (timestamp, hdkeypath,\n"
+            "                                                         hdseedid) and relation to the wallet (ismine, iswatchonly).\n"
+            "  \"iscompressed\" : true|false,        (boolean, optional) If the pubkey is compressed.\n"
+            "  \"label\" :  \"label\"                  (string) The label associated with the address. Defaults to \"\".\n"
+            "  \"timestamp\" : timestamp,            (number, optional) The creation time of the key if available, expressed in seconds since Epoch Time (Jan 1 1970 GMT).\n"
+            "  \"hdkeypath\" : \"keypath\"             (string, optional) The HD keypath, if the key is HD and available.\n"
+            "  \"hdseedid\" : \"<hash160>\"            (string, optional) The Hash160 of the HD seed.\n"
+            "  \"hdmasterfingerprint\" : \"<hash160>\" (string, optional) The fingerprint of the master key.\n"
             "  \"labels\"                            (object) Array of labels associated with the address.\n"
             "    [\n"
             "      { (json object of label data)\n"
-            "        \"name\": \"labelname\" (string) The label\n"
-            "        \"purpose\": \"string\" (string) Purpose of address (\"send\" for sending address, \"receive\" for receiving address)\n"
+            "        \"name\": \"label name\" (string) The label.\n"
+            "        \"purpose\": \"purpose\" (string) The purpose of the associated address (send or receive).\n"
             "      },...\n"
             "    ]\n"
             "}\n"