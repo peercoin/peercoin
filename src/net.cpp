0f1605dc79d5a2e432751823b9db9434d9618a3d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2020-07-09 10:12:19 +0300

    net: Log network activity status change unconditionally

--- src/net.cpp
+++ src/net.cpp
@@ -2255,7 +2255,7 @@ void Discover()
 
 void CConnman::SetNetworkActive(bool active)
 {
-    LogPrint(BCLog::NET, "SetNetworkActive: %s\n", active);
+    LogPrintf("%s: %s\n", __func__, active);
 
     if (fNetworkActive == active) {
         return;
