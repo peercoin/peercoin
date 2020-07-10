a5577f948f0ab4087e08b145fb6ed4d63867867e
John Newbery <john@johnnewbery.com>
2020-07-10 16:52:05 +0100

    [protocol] Remove unused GETHEADERS_VERSION
    GETHEADERS_VERSION is unused. No need to keep it in the codebase
    for just its historic comment.

--- src/version.h
+++ src/version.h
@@ -15,9 +15,6 @@ static const int OLD_VERSION = 70014; // peercoin: used to communicate with clie
 //! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
-//! In this version, 'getheaders' was introduced.
-static const int GETHEADERS_VERSION = 31800;
-
 //! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 70016;
 
