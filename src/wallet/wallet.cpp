3166a39b9142b7c6f0dbb51e7521ea5ec602e47e
backpacker69 <backpacker69@protonmail.com>
2020-07-29 19:01:40 +0300

    coinstake can be orphaned

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4105,7 +4105,8 @@ int CWalletTx::GetBlocksToMaturity() const
     if (!(IsCoinBase() || IsCoinStake()))
         return 0;
     int chain_depth = GetDepthInMainChain();
-    assert(chain_depth >= 0); // coinbase tx should not be conflicted
+    if (!IsCoinStake())
+        assert(chain_depth >= 0); // coinbase tx should not be conflicted
     return std::max(0, (Params().GetConsensus().nCoinbaseMaturity+1) - chain_depth);
 }
 
