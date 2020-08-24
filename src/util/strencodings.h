34610a2855d466efbeabc0ed62e2d82e54b36432
Vasil Dimov <vd@FreeBSD.org>
2020-08-24 21:34:26 +0200

    net: change CNetAddr::ip to have flexible size
    Before this change `CNetAddr::ip` was a fixed-size array of 16 bytes,
    not being able to store larger addresses (e.g. TORv3) and encoded
    smaller ones as 16-byte IPv6 addresses.
    
    Change its type to `prevector`, so that it can hold larger addresses and
    do not disguise non-IPv6 addresses as IPv6. So the IPv4 address
    `1.2.3.4` is now encoded as `01020304` instead of
    `00000000000000000000FFFF01020304`.
    
    Rename `CNetAddr::ip` to `CNetAddr::m_addr` because it is not an "IP" or
    "IP address" (TOR addresses are not IP addresses).
    
    In order to preserve backward compatibility with serialization (where
    e.g. `1.2.3.4` is serialized as `00000000000000000000FFFF01020304`)
    introduce `CNetAddr` dedicated legacy serialize/unserialize methods.
    
    Adjust `CSubNet` accordingly. Still use `CSubNet::netmask[]` of fixed 16
    bytes, but use the first 4 for IPv4 (not the last 4). Only allow
    subnetting for IPv4 and IPv6.
    
    Co-authored-by: Carl Dong <contact@carldong.me>

--- src/util/strencodings.h
+++ src/util/strencodings.h
@@ -202,6 +202,13 @@ std::optional<T> ToIntegral(const std::string& str)
 [[nodiscard]] bool ParseUInt16(const std::string& str, uint16_t* out);
 
 /**
+ * Convert decimal string to unsigned 8-bit integer with strict parse error feedback.
+ * @returns true if the entire string could be parsed as valid integer,
+ *   false if not the entire string could be parsed or when overflow or underflow occurred.
+ */
+NODISCARD bool ParseUInt8(const std::string& str, uint8_t *out);
+
+/**
  * Convert decimal string to unsigned 32-bit integer with strict parse error feedback.
  * @returns true if the entire string could be parsed as valid integer,
  *   false if not the entire string could be parsed or when overflow or underflow occurred.
