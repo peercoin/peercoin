df8720b94d501d33e5847991eb6d419a718b3fba
Carl Dong <contact@carldong.me>
2020-08-25 19:05:50 -0400

    validation: Move PruneOneBlockFile to BlockManager
    [META] This is a pure refactor commit.
    
    Move PruneBlockFile to BlockManager because:
    1. PruneOneBlockFile only acts on BlockManager
    2. Eliminates the need for callers (FindFilesToPrune{,Manual}) to have a
       reference to the larger ChainstateManager, just a reference to
       BlockManager is enough. See following commits.

--- src/validation.h
+++ src/validation.h
@@ -1034,9 +1034,6 @@ public:
      */
     bool ProcessNewBlockHeaders(const std::vector<CBlockHeader>& block, BlockValidationState& state, const CChainParams& chainparams, const CBlockIndex** ppindex = nullptr) LOCKS_EXCLUDED(cs_main);
 
-    //! Mark one block file as pruned (modify associated database entries)
-    void PruneOneBlockFile(const int fileNumber) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
-
     //! Load the block tree and coins database from disk, initializing state if we're running with -reindex
     bool LoadBlockIndex(const CChainParams& chainparams) EXCLUSIVE_LOCKS_REQUIRED(cs_main);
 
