7dceb3e31a26a263571e3f838c2870537ebcd6d5
backpacker69 <backpacker69@protonmail.com>
2020-08-27 18:18:53 +0300

    remove bitcoin residue

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -169,7 +169,7 @@ void BitcoinCore::shutdown()
 }
 
 static int qt_argc = 1;
-static const char* qt_argv = "bitcoin-qt";
+static const char* qt_argv = "peercoin-qt";
 
 BitcoinApplication::BitcoinApplication(interfaces::Node& node):
     QApplication(qt_argc, const_cast<char **>(&qt_argv)),
