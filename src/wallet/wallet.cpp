6fc3cc17c8484cde01fcc9f23b51f6eabcae986c
Andrew Chow <achow101-github@achow101.com>
2020-08-31 15:10:49 -0400

    Remove OutputGroup non-default constructors

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -4155,7 +4155,8 @@ std::vector<OutputGroup> CWallet::GroupOutputs(const std::vector<COutput>& outpu
                     gmap[dst].Insert(input_coin, output.nDepth, output.tx->IsFromMe(ISMINE_ALL), ancestors, descendants);
                 }
             } else {
-                groups.emplace_back(input_coin, output.nDepth, output.tx->IsFromMe(ISMINE_ALL), ancestors, descendants);
+                groups.emplace_back();
+                groups.back().Insert(input_coin, output.nDepth, output.tx->IsFromMe(ISMINE_ALL), ancestors, descendants);
             }
         }
     }
