332355583b77aa421412fa46b6b171ec650391ad
Zero <zero1729@protonmail.com>
2020-09-22 14:38:54 +0100

    refactor: took out unused member functions
    Took out the following unused member functions:
    
    - 'DBHeightKey()'
    - 'GetType()'
    - 'operator='

--- src/test/util_tests.cpp
+++ src/test/util_tests.cpp
@@ -2008,12 +2008,6 @@ struct Tracker
         copies = t.copies + 1;
         return *this;
     }
-    Tracker& operator=(Tracker&& t) noexcept
-    {
-        origin = t.origin;
-        copies = t.copies;
-        return *this;
-    }
 };
 
 }
