e6bbf6f08c40a9121e9e6cb2cb1d223c6ffcfda2
Suhas Daftuar <sdaftuar@gmail.com>
2020-10-24 03:16:04 -0400

    Avoid calling CAddrMan::Connected() on block-relay-only peer addresses
    Connected() updates the time we serve in addr messages, so avoid leaking
    block-relay-only peer connections by avoiding these calls.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -846,7 +846,8 @@ void PeerManager::ReattemptInitialBroadcast(CScheduler& scheduler) const
     scheduler.scheduleFromNow([&] { ReattemptInitialBroadcast(scheduler); }, delta);
 }
 
-void PeerManager::FinalizeNode(NodeId nodeid, bool& fUpdateConnectionTime) {
+void PeerManager::FinalizeNode(const CNode& node, bool& fUpdateConnectionTime) {
+    NodeId nodeid = node.GetId();
     fUpdateConnectionTime = false;
     LOCK(cs_main);
     int misbehavior{0};
@@ -863,7 +864,8 @@ void PeerManager::FinalizeNode(NodeId nodeid, bool& fUpdateConnectionTime) {
     if (state->fSyncStarted)
         nSyncStarted--;
 
-    if (misbehavior == 0 && state->fCurrentlyConnected) {
+    if (misbehavior == 0 && state->fCurrentlyConnected && !node.IsBlockOnlyConn()) {
+        // Note: we avoid changing visible addrman state for block-relay-only peers
         fUpdateConnectionTime = true;
     }
 
