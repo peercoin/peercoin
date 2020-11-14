dcb362cd01f54e0df6de6bdebadff74ac0321603
Kristaps Kaupe <kristaps@blogiem.lv>
2020-11-14 03:20:07 +0200

    Silence false positive GCC warning

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -3385,7 +3385,7 @@ static RPCHelpMan rescanblockchain()
     }
 
     int start_height = 0;
-    Optional<int> stop_height;
+    Optional<int> stop_height = MakeOptional(false, int());
     uint256 start_block;
     {
         LOCK(pwallet->cs_wallet);
