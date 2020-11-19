589dba3026810820fac3c945e1a9733f79c40189
Luke Dashjr <luke-jr+git@utopios.org>
2020-11-19 03:19:30 +0000

    GUI/Intro: Abstract GUI-to-option into Intro::getPrune

--- src/qt/intro.cpp
+++ src/qt/intro.cpp
@@ -160,6 +160,16 @@ void Intro::setDataDirectory(const QString &dataDir)
     }
 }
 
+int64_t Intro::getPruneMiB() const
+{
+    switch (ui->prune->checkState()) {
+    case Qt::Checked:
+        return PruneGBtoMiB(m_prune_target_gb);
+    case Qt::Unchecked: default:
+        return 0;
+    }
+}
+
 bool Intro::showIfNeeded(bool& did_show_intro, int64_t& prune_MiB)
 {
     did_show_intro = false;
