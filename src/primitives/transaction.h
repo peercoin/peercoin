89f384df399feeacaacafb09132986f241076bb8
MarcoFalke <falke.marco@gmail.com>
2020-12-10 11:21:33 +0100

    Move TX_MAX_STANDARD_VERSION to policy
    Also remove extraneous whitespace, should be reviewed with --ignore-all-space

--- src/primitives/transaction.h
+++ src/primitives/transaction.h
@@ -280,12 +280,6 @@ public:
     // Default transaction version.
     static const int32_t CURRENT_VERSION=3;
 
-    // Changing the default transaction version requires a two step process: first
-    // adapting relay policy by bumping MAX_STANDARD_VERSION, and then later date
-    // bumping the default CURRENT_VERSION at which point both CURRENT_VERSION and
-    // MAX_STANDARD_VERSION will be equal.
-    static const int32_t MAX_STANDARD_VERSION=3;
-
     // The local variables are made const to prevent unintended modification
     // without updating the cached hash value. However, CTransaction is not
     // actually immutable; deserialization and assignment are implemented,
