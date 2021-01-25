5521d01e59d703d30a6fe7a8167afe225460b282
MarcoFalke <falke.marco@gmail.com>
2021-01-25 12:22:03 +0100

    Move MakeNoLogFileContext to common libtest_util, and use it in bench
    Can be reviewed with --color-moved=dimmed-zebra

--- src/test/util/setup_common.h
+++ src/test/util/setup_common.h
@@ -184,6 +184,23 @@ std::unique_ptr<T> MakeNoLogFileContext(const std::string& chain_name = CBaseCha
     return std::make_unique<T>(chain_name, arguments);
 }
 
+/**
+ * Make a test setup that has disk access to the debug.log file disabled. Can
+ * be used in "hot loops", for example fuzzing or benchmarking.
+ */
+template <class T = const BasicTestingSetup>
+std::unique_ptr<T> MakeNoLogFileContext(const std::string& chain_name = CBaseChainParams::REGTEST, const std::vector<const char*>& extra_args = {})
+{
+    const std::vector<const char*> arguments = Cat(
+        {
+            "-nodebuglogfile",
+            "-nodebug",
+        },
+        extra_args);
+
+    return std::make_unique<T>(chain_name, arguments);
+}
+
 class CTxMemPoolEntry;
 
 struct TestMemPoolEntryHelper
