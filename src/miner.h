8572c6ac2226733bf07a3e7edc18e2ee7d5a4455
Carl Dong <contact@carldong.me>
2021-04-05 11:11:22 -0400

    miner: Pass in previous CBlockIndex to RegenerateCommitments

--- src/miner.h
+++ src/miner.h
@@ -210,8 +210,7 @@ namespace boost {
 
 void MintStake(boost::thread_group& threadGroup, std::shared_ptr<CWallet> pwallet, NodeContext* m_node);
 
-// TODO just accept a CBlockIndex*
 /** Update an old GenerateCoinbaseCommitment from CreateNewBlock after the block txs have changed */
-void RegenerateCommitments(CBlock& block, BlockManager& blockman);
+void RegenerateCommitments(CBlock& block, CBlockIndex* prev_block);
 
 #endif // BITCOIN_MINER_H
