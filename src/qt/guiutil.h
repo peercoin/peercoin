252877859d671d4c846e453f73aca9de9420329d
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-06-07 18:00:36 +0300

    qt: Add GUIUtil::ShowModalDialogAndDeleteOnClose

--- src/qt/guiutil.h
+++ src/qt/guiutil.h
@@ -41,6 +41,7 @@ class QAbstractButton;
 class QAbstractItemView;
 class QAction;
 class QDateTime;
+class QDialog;
 class QFont;
 class QKeySequence;
 class QLineEdit;
@@ -413,6 +414,11 @@ namespace GUIUtil
             },
             type);
     }
+
+    /**
+     * Shows a QDialog instance asynchronously, and deletes it on close.
+     */
+    void ShowModalDialogAndDeleteOnClose(QDialog* dialog);
 
 } // namespace GUIUtil
 
