b9af7e4266042b5d605ffd83f5497f12ba394d3e
Andrew Chow <achow101-github@achow101.com>
2021-06-23 16:54:13 -0400

    Use bilingual_str for address fetching functions
    For GetNewDestination, GetNewChangeDestination, and
    GetReservedDestination, use bilingual_str for
    errors

--- src/wallet/rpcwallet.cpp
+++ src/wallet/rpcwallet.cpp
@@ -272,9 +272,9 @@ static RPCHelpMan getnewaddress()
     }
 
     CTxDestination dest;
-    std::string error;
+    bilingual_str error;
     if (!pwallet->GetNewDestination(output_type, label, dest, error)) {
-        throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, error);
+        throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, error.original);
     }
 
     return EncodeDestination(dest);
@@ -317,9 +317,9 @@ static RPCHelpMan getrawchangeaddress()
     }
 
     CTxDestination dest;
-    std::string error;
+    bilingual_str error;
     if (!pwallet->GetNewChangeDestination(output_type, dest, error)) {
-        throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, error);
+        throw JSONRPCError(RPC_WALLET_KEYPOOL_RAN_OUT, error.original);
     }
     return EncodeDestination(dest);
 },
