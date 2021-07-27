456bf2dea8e79f42b1754ce72dbd282dc9bb5d0b
backpacker69 <backpacker69@protonmail.com>
2021-07-27 18:25:43 +0300

    get rid of prune residue

--- src/qt/optionsdialog.h
+++ src/qt/optionsdialog.h
@@ -59,7 +59,6 @@ private Q_SLOTS:
 
     void on_hideTrayIcon_stateChanged(int fState);
 
-    void togglePruneWarning(bool enabled);
     void showRestartWarning(bool fPersistent = false);
     void clearStatusLabel();
     void updateProxyValidationState();
