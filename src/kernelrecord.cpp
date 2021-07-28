b2d959b490110b55559be73a7ea98cc464bf82f3
backpacker69 <backpacker69@protonmail.com>
2021-07-28 20:45:18 +0300

    let segwit mint again

--- src/kernelrecord.cpp
+++ src/kernelrecord.cpp
@@ -45,8 +45,6 @@ vector<KernelRecord> KernelRecord::decomposeOutput(interfaces::Wallet& wallet, c
                 std::string addrStr;
 
                 if (ExtractDestination(txOut.scriptPubKey, address)) {
-                    if (address.type() != typeid(PKHash)) // only PKHash can mint at the moment
-                        continue;
                     // Sent to Bitcoin Address
                     addrStr = EncodeDestination(address);
                 } else {
