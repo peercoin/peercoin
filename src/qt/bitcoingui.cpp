afd997be897dc3b7b9f0510ac9692836e3e95008
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-08-11 20:30:49 +0300

    qt, refactor: Replace `if` check with `assert`
    There are no ways BitcoinGUI::updateWalletStatus being called without
    an instance of the WalletFrame class.

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -1458,9 +1458,8 @@ void BitcoinGUI::setEncryptionStatus(int status)
 
 void BitcoinGUI::updateWalletStatus()
 {
-    if (!walletFrame) {
-        return;
-    }
+    assert(walletFrame);
+
     WalletView * const walletView = walletFrame->currentWalletView();
     if (!walletView) {
         return;
