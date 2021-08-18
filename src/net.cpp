774955ebd36317f956c13e3a42cab9de796e42bf
fanquake <fanquake@gmail.com>
2021-08-18 13:37:27 +0800

    net: use clientInterface rather than uiInterface

--- src/net.cpp
+++ src/net.cpp
@@ -1306,8 +1306,9 @@ void CConnman::NotifyNumConnectionsChanged()
     }
     if(vNodesSize != nPrevNodeCount) {
         nPrevNodeCount = vNodesSize;
-        if(clientInterface)
+        if (clientInterface) {
             clientInterface->NotifyNumConnectionsChanged(vNodesSize);
+        }
     }
 }
 
@@ -2450,7 +2451,9 @@ void CConnman::SetNetworkActive(bool active)
 
     fNetworkActive = active;
 
-    uiInterface.NotifyNetworkActiveChanged(fNetworkActive);
+    if (clientInterface) {
+        clientInterface->NotifyNetworkActiveChanged(fNetworkActive);
+    }
 }
 
 CConnman::CConnman(uint64_t nSeed0In, uint64_t nSeed1In, CAddrMan& addrman_in, bool network_active)
@@ -2558,7 +2561,9 @@ bool CConnman::Start(CScheduler& scheduler, const Options& connOptions)
         LogPrintf("%i block-relay-only anchors will be tried for connections.\n", m_anchors.size());
     }
 
-    uiInterface.InitMessage(_("Starting network threads…").translated);
+    if (clientInterface) {
+        clientInterface->InitMessage(_("Starting network threads…").translated);
+    }
 
     fAddressesInitialized = true;
 
