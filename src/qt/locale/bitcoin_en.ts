6a7b24a86777537858d31632fedd031be49b07c9
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2021-08-22 00:03:31 +0300

    qt: Handle new added plurals in bitcoin_en.ts
    This step was missed. See translation_process.md

--- src/qt/locale/bitcoin_en.ts
+++ src/qt/locale/bitcoin_en.ts
@@ -780,8 +780,8 @@
         <source>%n active connection(s) to Bitcoin network.</source>
         <extracomment>A substring of the tooltip.</extracomment>
         <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+            <numerusform>%n active connection to Bitcoin network.</numerusform>
+            <numerusform>%n active connections to Bitcoin network.</numerusform>
         </translation>
     </message>
     <message>
@@ -1368,8 +1368,8 @@
         <source>(sufficient to restore backups %n day(s) old)</source>
         <extracomment>Explanatory text on the capability of the current prune target.</extracomment>
         <translation type="unfinished">
-            <numerusform></numerusform>
-            <numerusform></numerusform>
+            <numerusform>(sufficient to restore backups %n day old)</numerusform>
+            <numerusform>(sufficient to restore backups %n days old)</numerusform>
         </translation>
     </message>
         <translation type="unfinished"></translation>
