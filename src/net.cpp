629eea72d677ed627d13255c561f3b79e96d4813
Sebastian Falbesoner <sebastian.falbesoner@gmail.com>
2021-08-28 19:51:08 +0200

    refactor: remove RecursiveMutex cs_totalBytesRecv, use std::atomic instead
    The RecursiveMutex cs_totalBytesRecv is only used at two places: in
    CConnman::RecordBytesRecv() to increment the nTotalBytesRecv member, and in
    CConnman::GetTotalBytesRecv() to read it. For this simple use-case, we can
    make the member std::atomic instead to achieve the same result.

--- src/net.cpp
+++ src/net.cpp
@@ -2881,7 +2881,6 @@ bool CConnman::DisconnectNode(NodeId id)
 
 void CConnman::RecordBytesRecv(uint64_t bytes)
 {
-    LOCK(cs_totalBytesRecv);
     nTotalBytesRecv += bytes;
 }
 
@@ -2958,7 +2957,6 @@ uint64_t CConnman::GetOutboundTargetBytesLeft() const
 
 uint64_t CConnman::GetTotalBytesRecv() const
 {
-    LOCK(cs_totalBytesRecv);
     return nTotalBytesRecv;
 }
 
