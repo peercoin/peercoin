5cec93849a0f187edc5172a9b738bf368d210f89
MarcoFalke <falke.marco@gmail.com>
2021-11-15 13:42:00 +0100

    scripted-diff: Move miner to src/node
    -BEGIN VERIFY SCRIPT-
     # Move module
     git mv src/miner.cpp src/node/
     git mv src/miner.h   src/node/
     # Replacements
     sed -i 's:miner\.h:node/miner.h:g'     $(git grep -l miner)
     sed -i 's:miner\.cpp:node/miner.cpp:g' $(git grep -l miner)
     sed -i 's:MINER_H:NODE_MINER_H:g'      $(git grep -l MINER_H)
    -END VERIFY SCRIPT-

--- src/node/miner.h
+++ src/node/miner.h
@@ -3,8 +3,8 @@
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
-#ifndef BITCOIN_MINER_H
-#define BITCOIN_MINER_H
+#ifndef BITCOIN_NODE_MINER_H
+#define BITCOIN_NODE_MINER_H
 
 #include <primitives/block.h>
 #include <txmempool.h>
@@ -213,4 +213,4 @@ void MintStake(boost::thread_group& threadGroup, std::shared_ptr<CWallet> pwalle
 /** Update an old GenerateCoinbaseCommitment from CreateNewBlock after the block txs have changed */
 void RegenerateCommitments(CBlock& block, ChainstateManager& chainman);
 
-#endif // BITCOIN_MINER_H
+#endif // BITCOIN_NODE_MINER_H
