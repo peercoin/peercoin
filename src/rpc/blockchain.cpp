db42f33708a3efdcc30ad6ff3de75854e40c09cf
MarcoFalke <falke.marco@gmail.com>
2022-01-02 10:41:14 +0100

    rpc: Use EnsureAnyArgsman in rpc/blockchain

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -1467,6 +1467,7 @@ RPCHelpMan getblockchaininfo()
                 },
         [&](const RPCHelpMan& self, const JSONRPCRequest& request) -> UniValue
 {
+    const ArgsManager& args{EnsureAnyArgsman(request.context)};
     ChainstateManager& chainman = EnsureAnyChainman(request.context);
     CChainState& active_chainstate = chainman.ActiveChainstate();
 
