e1a4853efea1dc5e99c19fa8893aad3065ae8d15
Marius Kjærstad <mkjaerstad@protonmail.com>
2022-01-06 05:19:01 +0100

    Update of copyright year to 2022

--- src/qt/mintingview.cpp
+++ src/qt/mintingview.cpp
@@ -1,4 +1,4 @@
-// Copyright (c) 2012-2021 The Peercoin developers
+// Copyright (c) 2012-2022 The Peercoin developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 #include <qt/mintingview.h>
