7291df1522d03f02f7de82a3ac0087d69fd114eb
Jon Atack <jon@atack.com>
2022-04-01 12:23:01 +0200

    Put lock logging behind DEBUG_LOCKCONTENTION preprocessor directive
    Github-Pull: #24770
    Rebased-From: 39a34b6877945908759f6a2322f60852e521e2ee

--- src/net.h
+++ src/net.h
@@ -13,6 +13,7 @@
 #include <crypto/siphash.h>
 #include <hash.h>
 #include <i2p.h>
+#include <logging.h>
 #include <net_permissions.h>
 #include <netaddress.h>
 #include <netbase.h>
@@ -31,6 +32,7 @@
 #include <cstdint>
 #include <deque>
 #include <functional>
+#include <list>
 #include <map>
 #include <memory>
 #include <optional>
