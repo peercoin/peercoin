49d980a5dceb001a9cb31317b318c8b2392bfb94
furszy <matiasfurszyfer@protonmail.com>
2022-04-27 10:52:30 -0300

    wallet: IsSpent, 'COutPoint' arg instead of (hash, index)

--- src/wallet/spend.cpp
+++ src/wallet/spend.cpp
@@ -151,7 +151,7 @@ CoinsResult AvailableCoins(const CWallet& wallet,
             if (wallet.IsLockedCoin(outpoint))
                 continue;
 
-            if (wallet.IsSpent(wtxid, i))
+            if (wallet.IsSpent(outpoint))
                 continue;
 
             isminetype mine = wallet.IsMine(output);
