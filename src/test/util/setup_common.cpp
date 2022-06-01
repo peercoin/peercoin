bf9c8b2bb96631a2beeda7e643139549cb445060
Jon Atack <jon@atack.com>
2022-06-01 13:44:59 +0200

    Create BCLog::Level::Trace log severity level
    for verbose log messages for development or debugging only, as bitcoind may run
    more slowly, that are more granular/frequent than the Debug log level, i.e. for
    very high-frequency, low-level messages to be logged distinctly from
    higher-level, less-frequent debug logging that could still be usable in production.
    
    An example would be to log higher-level peer events (connection, disconnection,
    misbehavior, eviction) as Debug, versus Trace for low-level, high-volume p2p
    messages in the BCLog::NET category. This will enable the user to log only the
    former without the latter, in order to focus on high-level peer management events.
    
    With respect to the name, "trace" is suggested as the most granular level
    in resources like the following:
    - https://sematext.com/blog/logging-levels
    - https://howtodoinjava.com/log4j2/logging-levels
    
    Update the test framework and add test coverage.

--- src/test/util/setup_common.cpp
+++ src/test/util/setup_common.cpp
@@ -113,7 +113,7 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName, const std::ve
             "-logsourcelocations",
             "-logtimemicros",
             "-logthreadnames",
-            "-loglevel=debug",
+            "-loglevel=trace",
             "-debug",
             "-debugexclude=libevent",
             "-debugexclude=leveldb",
