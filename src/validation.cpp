40a5fdbb8e9f4fe8ba800a2eac602ec86e8721c8
backpacker69 <backpacker69@protonmail.com>
2022-06-04 11:03:16 +1000

    build fixes

--- src/validation.cpp
+++ src/validation.cpp
@@ -2021,6 +2021,9 @@ bool CChainState::ConnectBlock(const CBlock& block, BlockValidationState& state,
                             tx_state.GetRejectReason(), tx_state.GetDebugMessage());
                 return error("%s: Consensus::CheckTxInputs: %s, %s", __func__, tx.GetHash().ToString(), state.ToString());
             }
+            nValueIn = 0;
+            for (unsigned int i = 0; i < tx.vin.size(); i++)
+                nValueIn += view.AccessCoin(tx.vin[i].prevout).out.nValue;
             nValueIn += view.GetValueIn(tx);
             nValueOut += tx.GetValueOut();
             if (!tx.IsCoinStake())
