61a91b16616bd9d438d19f5c21e3795caba30811
MacroFake <falke.marco@gmail.com>
2022-06-06 17:22:59 +0200

    Use AutoFile where possible

--- src/rpc/blockchain.cpp
+++ src/rpc/blockchain.cpp
@@ -2586,7 +2586,7 @@ static RPCHelpMan dumptxoutset()
     }
 
     FILE* file{fsbridge::fopen(temppath, "wb")};
-    CAutoFile afile{file, SER_DISK, CLIENT_VERSION};
+    AutoFile afile{file};
     if (afile.IsNull()) {
         throw JSONRPCError(
             RPC_INVALID_PARAMETER,
@@ -2607,7 +2607,7 @@ static RPCHelpMan dumptxoutset()
 UniValue CreateUTXOSnapshot(
     NodeContext& node,
     CChainState& chainstate,
-    CAutoFile& afile,
+    AutoFile& afile,
     const fs::path& path,
     const fs::path& temppath)
 {
