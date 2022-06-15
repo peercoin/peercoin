bitcoin-ghost/bitcoin-core-ladder-script

RPC: Rephrase generatetoaddress help, and use PACKAGE_NAME

src/rpc/mining.cpp
@@ -252,7 +252,7 @@ static UniValue generatetoaddress(const JSONRPCRequest& request)
                 RPCExamples{
             "\nGenerate 11 blocks to myaddress\n"
             + HelpExampleCli("generatetoaddress", "11 \"myaddress\"")
-            + "If you are running the bitcoin core wallet, you can get a new address to send the newly generated bitcoin to with:\n"
+            + "If you are using the " PACKAGE_NAME " wallet, you can get a new address to send the newly generated bitcoin to with:\n"
             + HelpExampleCli("getnewaddress", "")
                 },
             }.Check(request);

peercoin/peercoin

src/rpc/mining.cpp
@@ -298,7 +298,7 @@ static UniValue generatetoaddress(const JSONRPCRequest& request)
                 RPCExamples{
             "\nGenerate 11 blocks to myaddress\n"
             + HelpExampleCli("generatetoaddress", "11 \"myaddress\"")
-            + "If you are running the Peercoin wallet, you can get a new address to send the newly generated peercoin to with:\n"
+            + "If you are using the " PACKAGE_NAME " wallet, you can get a new address to send the newly generated peercoin to with:\n"
             + HelpExampleCli("getnewaddress", "")
                 },
             }.Check(request);