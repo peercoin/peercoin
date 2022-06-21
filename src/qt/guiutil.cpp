05b851f07b07baaa49066cfc0b160e22614136b8
w0xlt <94266259+w0xlt@users.noreply.github.com>
2022-06-21 19:11:54 -0300

    qt: Replace `QRegExp` with `QRegularExpression`
    Co-authored-by: Pavol Rusnak <pavol@rusnak.io>
    Co-authored-by: Jarol Rodriguez <jarolrod@tutanota.com>

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -53,6 +53,7 @@
 #include <QMouseEvent>
 #include <QPluginLoader>
 #include <QProgressDialog>
+#include <QRegularExpression>
 #include <QScreen>
 #include <QSettings>
 #include <QShortcut>
@@ -283,10 +284,11 @@ QString getDefaultDataDirectory()
 
 QString ExtractFirstSuffixFromFilter(const QString& filter)
 {
-    QRegExp filter_re(".* \\(\\*\\.(.*)[ \\)]");
+    QRegularExpression filter_re(QStringLiteral(".* \\(\\*\\.(.*)[ \\)]"), QRegularExpression::InvertedGreedinessOption);
     QString suffix;
-    if (filter_re.exactMatch(filter)) {
-        suffix = filter_re.cap(1);
+    QRegularExpressionMatch m = filter_re.match(filter);
+    if (m.hasMatch()) {
+        suffix = m.captured(1);
     }
     return suffix;
 }
