bitcoin-ghost/bitcoin-core-ladder-script

doc: Fix RPC result documentation

src/rpc/misc.cpp
@@ -44,10 +44,10 @@ static RPCHelpMan validateaddress()
                     RPCResult::Type::OBJ, "", "",
                     {
                         {RPCResult::Type::BOOL, "isvalid", "If the address is valid or not"},
-                        {RPCResult::Type::STR, "address", "The bitcoin address validated"},
-                        {RPCResult::Type::STR_HEX, "scriptPubKey", "The hex-encoded scriptPubKey generated by the address"},
-                        {RPCResult::Type::BOOL, "isscript", "If the key is a script"},
-                        {RPCResult::Type::BOOL, "iswitness", "If the address is a witness address"},
+                        {RPCResult::Type::STR, "address", /* optional */ true, "The bitcoin address validated"},
+                        {RPCResult::Type::STR_HEX, "scriptPubKey", /* optional */ true, "The hex-encoded scriptPubKey generated by the address"},
+                        {RPCResult::Type::BOOL, "isscript", /* optional */ true, "If the key is a script"},
+                        {RPCResult::Type::BOOL, "iswitness", /* optional */ true, "If the address is a witness address"},
                         {RPCResult::Type::NUM, "witness_version", /* optional */ true, "The version number of the witness program"},
                         {RPCResult::Type::STR_HEX, "witness_program", /* optional */ true, "The hex value of the witness program"},
                         {RPCResult::Type::STR, "error", /* optional */ true, "Error message, if any"},
@@ -717,7 +717,7 @@ static RPCHelpMan getindexinfo()
                     {"index_name", RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "Filter results for an index with a specific name."},
                 },
                 RPCResult{
-                    RPCResult::Type::OBJ, "", "", {
+                    RPCResult::Type::OBJ_DYN, "", "", {
                         {
                             RPCResult::Type::OBJ, "name", "The name of the index",
                             {

peercoin/peercoin

src/rpc/misc.cpp
@@ -44,10 +44,10 @@ static RPCHelpMan validateaddress()
                     RPCResult::Type::OBJ, "", "",
                     {
                         {RPCResult::Type::BOOL, "isvalid", "If the address is valid or not"},
-                        {RPCResult::Type::STR, "address", "The peercoin address validated"},
-                        {RPCResult::Type::STR_HEX, "scriptPubKey", "The hex-encoded scriptPubKey generated by the address"},
-                        {RPCResult::Type::BOOL, "isscript", "If the key is a script"},
-                        {RPCResult::Type::BOOL, "iswitness", "If the address is a witness address"},
+                        {RPCResult::Type::STR, "address", /* optional */ true, "The peercoin address validated"},
+                        {RPCResult::Type::STR_HEX, "scriptPubKey", /* optional */ true, "The hex-encoded scriptPubKey generated by the address"},
+                        {RPCResult::Type::BOOL, "isscript", /* optional */ true, "If the key is a script"},
+                        {RPCResult::Type::BOOL, "iswitness", /* optional */ true, "If the address is a witness address"},
                         {RPCResult::Type::NUM, "witness_version", /* optional */ true, "The version number of the witness program"},
                         {RPCResult::Type::STR_HEX, "witness_program", /* optional */ true, "The hex value of the witness program"},
                         {RPCResult::Type::STR, "error", /* optional */ true, "Error message, if any"},
@@ -717,7 +717,7 @@ static RPCHelpMan getindexinfo()
                     {"index_name", RPCArg::Type::STR, RPCArg::Optional::OMITTED_NAMED_ARG, "Filter results for an index with a specific name."},
                 },
                 RPCResult{
-                    RPCResult::Type::OBJ, "", "", {
+                    RPCResult::Type::OBJ_DYN, "", "", {
                         {
                             RPCResult::Type::OBJ, "name", "The name of the index",
                             {