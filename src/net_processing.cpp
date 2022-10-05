2bc775382254d6d67da5d37414b9439bb11bdd75
Anthony Towns <aj@erisian.com.au>
2022-10-05 19:12:14 +1000

    net_processing: Localise orphan_work_set handling to ProcessOrphanTx

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -591,8 +591,9 @@ private:
      *                     orphan will be reconsidered on each call of this function. This set
      *                     may be added to if accepting an orphan causes its children to be
      *                     reconsidered.
+     * @return             True if there are still orphans in this peer's work set.
      */
-    void ProcessOrphanTx(Peer& peer) EXCLUSIVE_LOCKS_REQUIRED(cs_main, g_cs_orphans)
+    bool ProcessOrphanTx(Peer& peer) EXCLUSIVE_LOCKS_REQUIRED(cs_main, g_cs_orphans)
         EXCLUSIVE_LOCKS_REQUIRED(!m_peer_mutex, g_msgproc_mutex);
     /** Process a single headers message from a peer.
      *
@@ -3015,11 +3016,13 @@ void PeerManagerImpl::ProcessHeadersMessage(CNode& pfrom, Peer& peer,
     return;
 }
 
-void PeerManagerImpl::ProcessOrphanTx(Peer& peer)
+bool PeerManagerImpl::ProcessOrphanTx(Peer& peer)
 {
     AssertLockHeld(g_msgproc_mutex);
     AssertLockHeld(cs_main);
     AssertLockHeld(g_cs_orphans);
+
+    if (peer.m_orphan_work_set.empty()) return false;
 
     while (!peer.m_orphan_work_set.empty()) {
         const uint256 orphanHash = *peer.m_orphan_work_set.begin();
@@ -3085,6 +3088,8 @@ void PeerManagerImpl::ProcessOrphanTx(Peer& peer)
             break;
         }
     }
+
+    return !peer.m_orphan_work_set.empty();
 }
 
 bool PeerManagerImpl::PrepareBlockFilterRequest(CNode& node, Peer& peer,
@@ -5109,26 +5114,22 @@ bool PeerManagerImpl::ProcessMessages(CNode* pfrom, std::atomic<bool>& interrupt
         }
     }
 
+    bool has_more_orphans;
     {
         LOCK2(cs_main, g_cs_orphans);
-        if (!peer->m_orphan_work_set.empty()) {
-            ProcessOrphanTx(*peer);
-        }
+        has_more_orphans = ProcessOrphanTx(*peer);
     }
 
     if (pfrom->fDisconnect)
         return false;
+
+    if (has_more_orphans) return true;
 
     // this maintains the order of responses
     // and prevents m_getdata_requests to grow unbounded
     {
         LOCK(peer->m_getdata_requests_mutex);
         if (!peer->m_getdata_requests.empty()) return true;
-    }
-
-    {
-        LOCK(g_cs_orphans);
-        if (!peer->m_orphan_work_set.empty()) return true;
     }
 
     // Don't bother if send buffer is too full to respond anyway
