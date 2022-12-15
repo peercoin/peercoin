b0eae01d1808d446bfd587eadd8a7ac9e89b610f
Aurèle Oulès <aurele@oules.com>
2022-12-15 10:58:14 +0100

    rpc: Sanitize label name in various RPCs
    - importprivkey
    - importaddress
    - importpubkey
    - listtransactions
    - listsinceblock
    - importmulti
    - importdescriptors

--- src/wallet/rpc/backup.cpp
+++ src/wallet/rpc/backup.cpp
@@ -162,7 +162,7 @@ RPCHelpMan importprivkey()
         std::string strSecret = request.params[0].get_str();
         std::string strLabel;
         if (!request.params[1].isNull())
-            strLabel = request.params[1].get_str();
+            strLabel = LabelFromValue(request.params[1]);
 
         // Whether to perform rescan after import
         if (!request.params[2].isNull())
@@ -252,7 +252,7 @@ RPCHelpMan importaddress()
 
     std::string strLabel;
     if (!request.params[1].isNull())
-        strLabel = request.params[1].get_str();
+        strLabel = LabelFromValue(request.params[1]);
 
     // Whether to perform rescan after import
     bool fRescan = true;
@@ -345,7 +345,7 @@ RPCHelpMan importpubkey()
 
     std::string strLabel;
     if (!request.params[1].isNull())
-        strLabel = request.params[1].get_str();
+        strLabel = LabelFromValue(request.params[1]);
 
     // Whether to perform rescan after import
     bool fRescan = true;
@@ -1064,7 +1064,7 @@ static UniValue ProcessImport(CWallet& wallet, const UniValue& data, const int64
         if (internal && data.exists("label")) {
             throw JSONRPCError(RPC_INVALID_PARAMETER, "Internal addresses should not have a label");
         }
-        const std::string& label = data.exists("label") ? data["label"].get_str() : "";
+        const std::string& label = data.exists("label") ? LabelFromValue(data["label"]) : "";
         const bool add_keypool = data.exists("keypool") ? data["keypool"].get_bool() : false;
 
         // Add to keypool only works with privkeys disabled
@@ -1358,7 +1358,7 @@ static UniValue ProcessDescriptorImport(CWallet& wallet, const UniValue& data, c
         const std::string& descriptor = data["desc"].get_str();
         const bool active = data.exists("active") ? data["active"].get_bool() : false;
         const bool internal = data.exists("internal") ? data["internal"].get_bool() : false;
-        const std::string& label = data.exists("label") ? data["label"].get_str() : "";
+        const std::string& label = data.exists("label") ? LabelFromValue(data["label"]) : "";
 
         // Parse descriptor string
         FlatSigningProvider keys;
