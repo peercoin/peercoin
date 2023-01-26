3ae76121938189bd0a83f044c1c4010a94d4ebbd
John Moffett <john.moff@gmail.com>
2023-01-26 10:19:11 -0500

    Correctly limit overview transaction list
    The way that the main overview page limits the number
    of transactions displayed (currently 5) is not
    an appropriate use of Qt. If it's run with a DEBUG
    build of Qt, it'll result in a segfault in certain
    relatively common situations. Instead of artificially
    limiting the rowCount() in the subclassed proxy
    filter, we hide/unhide the rows in the displaying
    QListView upon any changes in the sorted proxy filter.

--- src/qt/overviewpage.h
+++ src/qt/overviewpage.h
@@ -61,6 +61,7 @@ private:
     std::unique_ptr<TransactionFilterProxy> filter;
 
 private Q_SLOTS:
+    void LimitTransactionRows();
     void updateDisplayUnit();
     void handleTransactionClicked(const QModelIndex &index);
     void updateAlerts(const QString &warnings);
