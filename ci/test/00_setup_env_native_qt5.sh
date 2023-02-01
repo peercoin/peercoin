c21e416f6c0b7bd8efb3f6d0a5ae07a976e22060
fanquake <fanquake@gmail.com>
2023-02-01 11:11:40 +0000

    ci: avoid using -Werror for older compilers
    Don't enable `-Werror` (in the CI) for compilers at least older than
    our current release compiler (GCC 10). It provides little-to-no value,
    other than turning compiler bugs & false positives into build failures,
    and we aren't going to mutate perfectly fine/working code, for the sake
    of avoid a warning that shouldn't even exist.
    
    I also do not see the point of playing whack-a-mole and turning off various
    warnings/trying to further work around the broken compiler, just to
    acheive warningless builds for the sake of warningless builds.
    
    One anecdote from "How SQLite Is Tested":
    > Static analysis has found a few bugs in SQLite, but those are the
    > exceptions. More bugs have been introduced into SQLite while trying
    > to get it to compile without warnings than have been found by static
    > analysis.
    
    https://www.sqlite.org/testing.html.

--- ci/test/00_setup_env_native_qt5.sh
+++ ci/test/00_setup_env_native_qt5.sh
@@ -16,6 +16,7 @@ export RUN_SECURITY_TESTS="true"
 export RUN_UNIT_TESTS_SEQUENTIAL="true"
 export RUN_UNIT_TESTS="false"
 export GOAL="install"
+export NO_WERROR=1
 export DOWNLOAD_PREVIOUS_RELEASES="true"
 export BITCOIN_CONFIG="--enable-zmq --with-libs=no --with-gui=qt5 --enable-reduce-exports \
 --enable-debug CFLAGS=\"-g0 -O2 -funsigned-char\" CXXFLAGS=\"-g0 -O2 -funsigned-char\" CC=gcc-8 CXX=g++-8"
