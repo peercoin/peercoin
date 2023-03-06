bfadcedb4139a2d687c16bd362cfd5770bc3b61d
Ben Woosley <ben.woosley@gmail.com>
2023-03-06 17:22:33 +0100

    refactor: Extract util/exception from util/system
    This is a minimal extraction of a single function, but also the only use
    of std::exception in util/system.
    
    The background of this commit is an ongoing effort to decouple the
    libbitcoinkernel library from the ArgsManager defined in system.h.
    Moving the function out of system.h allows including it from a separate
    source file without including the ArgsManager definitions from system.h.

--- src/bitcoin-cli.cpp
+++ src/bitcoin-cli.cpp
@@ -16,6 +16,7 @@
 #include <rpc/request.h>
 #include <tinyformat.h>
 #include <univalue.h>
+#include <util/exception.h>
 #include <util/strencodings.h>
 #include <util/system.h>
 #include <util/translation.h>
