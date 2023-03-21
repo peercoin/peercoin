59fc37ca28c8c3c47725de5cd86665e5974870ab
Hennadii Stepanov <32963518+hebasto@users.noreply.github.com>
2023-03-21 13:04:01 +0000

    refactor: Make move semantics explicit for callers

--- src/validation.cpp
+++ src/validation.cpp
@@ -2468,7 +2468,7 @@ bool Chainstate::ConnectBlock(const CBlock& block, BlockValidationState& state, 
                 return error("ConnectBlock(): CheckInputScripts on %s failed with %s",
                     tx.GetHash().ToString(), state.ToString());
             }
-            control.Add(vChecks);
+            control.Add(std::move(vChecks));
         }
 
         CTxUndo undoDummy;
