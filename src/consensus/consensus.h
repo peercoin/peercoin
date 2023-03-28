d51d6110cf67e8cdc5c4b388fc8462ad679507db
backpacker69 <backpacker69@protonmail.com>
2023-03-28 15:54:25 +1100

    reduce WITNESS_SCALE_FACTOR to remove witness preferences

--- src/consensus/consensus.h
+++ src/consensus/consensus.h
@@ -17,8 +17,8 @@ static const unsigned int MAX_BLOCK_WEIGHT = 4000000;
 static const int64_t MAX_BLOCK_SIGOPS_COST = 80000;
 /** Coinbase transaction outputs can only be spent after this number of new blocks (network rule) */
 static const int COINBASE_MATURITY = 100;
-
-static const int WITNESS_SCALE_FACTOR = 4;
+/** Peercoin does not impose witness preference */
+static const int WITNESS_SCALE_FACTOR = 1;
 
 static const size_t MIN_TRANSACTION_WEIGHT = WITNESS_SCALE_FACTOR * 60; // 60 is the lower bound for the size of a valid serialized CTransaction
 static const size_t MIN_SERIALIZABLE_TRANSACTION_WEIGHT = WITNESS_SCALE_FACTOR * 10; // 10 is the lower bound for the size of a serialized CTransaction
