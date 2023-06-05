bitcoincore-dev/bitcoin-2001-863168-294116-d812cf1189

Use `PACKAGE_NAME` in messages rather than hardcoding "Bitcoin Core"

src/qt/optionsdialog.cpp
@@ -92,6 +92,7 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
     }
 
 #ifndef ENABLE_EXTERNAL_SIGNER
+    ui->externalSignerPath->setText(ui->externalSignerPath->text().arg(PACKAGE_NAME));
     //: "External signing" means using devices such as hardware wallets.
     ui->externalSignerPath->setToolTip(tr("Compiled without external signing support (required for external signing)"));
     ui->externalSignerPath->setEnabled(false);

peercoin/peercoin

src/qt/optionsdialog.cpp
@@ -86,7 +86,9 @@ OptionsDialog::OptionsDialog(QWidget *parent, bool enableWallet) :
         ui->thirdPartyTxUrls->setVisible(false);
     }
 
-#ifndef ENABLE_EXTERNAL_SIGNER
+#ifdef ENABLE_EXTERNAL_SIGNER
+    ui->externalSignerPath->setToolTip(ui->externalSignerPath->toolTip().arg(PACKAGE_NAME));
+#else
     //: "External signing" means using devices such as hardware wallets.
     ui->externalSignerPath->setToolTip(tr("Compiled without external signing support (required for external signing)"));
     ui->externalSignerPath->setEnabled(false);