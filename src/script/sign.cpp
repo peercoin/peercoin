darosior/bitcoin

script/sign: signing support for Miniscript with timelocks

src/script/sign.cpp
@@ -378,9 +378,9 @@ struct Satisfier {
     }
 
     //! Time lock satisfactions.
-    // TODO
-    bool CheckAfter(uint32_t value) const { return false; }
-    bool CheckOlder(uint32_t value) const { return false; }
+    bool CheckAfter(uint32_t value) const { return creator.Checker().CheckLockTime(CScriptNum(value)); }
+    bool CheckOlder(uint32_t value) const { return creator.Checker().CheckSequence(CScriptNum(value)); }
+
 
     //! Hash preimage satisfactions.
     // TODO
@@ -615,6 +615,8 @@ public:
     DummySignatureChecker() {}
     bool CheckECDSASignature(const std::vector<unsigned char>& sig, const std::vector<unsigned char>& vchPubKey, const CScript& scriptCode, SigVersion sigversion) const override { return sig.size() != 0; }
     bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, const ScriptExecutionData& execdata, ScriptError* serror) const override { return sig.size() != 0; }
+    bool CheckLockTime(const CScriptNum& nLockTime) const override { return true; }
+    bool CheckSequence(const CScriptNum& nSequence) const override { return true; }
 };
 const DummySignatureChecker DUMMY_CHECKER;
 

peercoin/peercoin

src/script/sign.cpp
@@ -438,9 +438,9 @@ struct Satisfier {
     }
 
     //! Time lock satisfactions.
-    // TODO
-    bool CheckAfter(uint32_t value) const { return false; }
-    bool CheckOlder(uint32_t value) const { return false; }
+    bool CheckAfter(uint32_t value) const { return m_creator.Checker().CheckLockTime(CScriptNum(value)); }
+    bool CheckOlder(uint32_t value) const { return m_creator.Checker().CheckSequence(CScriptNum(value)); }
+
 
     //! Hash preimage satisfactions.
     // TODO
@@ -675,6 +675,8 @@ public:
     DummySignatureChecker() = default;
     bool CheckECDSASignature(const std::vector<unsigned char>& sig, const std::vector<unsigned char>& vchPubKey, const CScript& scriptCode, SigVersion sigversion) const override { return sig.size() != 0; }
     bool CheckSchnorrSignature(Span<const unsigned char> sig, Span<const unsigned char> pubkey, SigVersion sigversion, ScriptExecutionData& execdata, ScriptError* serror) const override { return sig.size() != 0; }
+    bool CheckLockTime(const CScriptNum& nLockTime) const override { return true; }
+    bool CheckSequence(const CScriptNum& nSequence) const override { return true; }
 };
 }
 