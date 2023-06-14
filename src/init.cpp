f42d40f5284e500ab3e6502229b633128085a8cc
ismaelsadeeq <ask4ismailsadiq@gmail.com>
2023-06-14 22:39:26 +0100

    tx fees, policy: read stale fee estimates with a regtest-only option
    If -acceptstalefeeestimates option is passed stale fee estimates can now
    be read when operating in regtest environments.
    
    Additionally, this commit updates all declarations of the CBlockPolicyEstimator
    class to include a the second constructor variable.
    
    Github-Pull: #27622
    Rebased-From: cf219f29f3c5b41070eaab9a549a476f01990f3a

--- src/init.cpp
+++ src/init.cpp
@@ -1254,7 +1254,11 @@ bool AppInitMain(NodeContext& node, interfaces::BlockAndHeaderTipInfo* tip_info)
     // Don't initialize fee estimation with old data if we don't relay transactions,
     // as they would never get updated.
     if (!ignores_incoming_txs) {
-        node.fee_estimator = std::make_unique<CBlockPolicyEstimator>(FeeestPath(args));
+        bool read_stale_estimates = args.GetBoolArg("-acceptstalefeeestimates", DEFAULT_ACCEPT_STALE_FEE_ESTIMATES);
+        if (read_stale_estimates && (chainparams.NetworkIDString() != CBaseChainParams::REGTEST)) {
+            return InitError(strprintf(_("acceptstalefeeestimates is not supported on %s chain."), chainparams.NetworkIDString()));
+        }
+        node.fee_estimator = std::make_unique<CBlockPolicyEstimator>(FeeestPath(args), read_stale_estimates);
 
         // Flush estimates to disk periodically
         CBlockPolicyEstimator* fee_estimator = node.fee_estimator.get();
