f3726eb71c7ec16e31ce4e63dd51b47f2c25ce77
lateminer <9951982+lateminer@users.noreply.github.com>
2023-11-04 21:41:17 +0100

    Remove unused nPruneAfterHeight variable

--- src/kernel/chainparams.h
+++ src/kernel/chainparams.h
@@ -109,7 +109,6 @@ public:
     bool IsTestChain() const { return m_is_test_chain; }
     /** If this chain allows time to be mocked */
     bool IsMockableChain() const { return m_is_mockable_chain; }
-    uint64_t PruneAfterHeight() const { return nPruneAfterHeight; }
     /** Minimum free space (in GB) needed for data directory */
     uint64_t AssumedBlockchainSize() const { return m_assumed_blockchain_size; }
     /** Minimum free space (in GB) needed for data directory when pruned; Does not include prune target*/
@@ -168,7 +167,6 @@ protected:
     Consensus::Params consensus;
     CMessageHeader::MessageStartChars pchMessageStart;
     uint16_t nDefaultPort;
-    uint64_t nPruneAfterHeight;
     uint64_t m_assumed_blockchain_size;
     uint64_t m_assumed_chain_state_size;
     std::vector<std::string> vSeeds;
