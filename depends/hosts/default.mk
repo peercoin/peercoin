15d821d87aeb7e1013cabcffd0fc6fba58b6b597
backpacker69 <backpacker69@protonmail.com>
2023-11-07 12:54:24 +1100

    upgrade openssl to 1.1.1t

--- depends/hosts/default.mk
+++ depends/hosts/default.mk
@@ -11,6 +11,8 @@ default_host_LIBTOOL = $(host_toolchain)libtool
 default_host_INSTALL_NAME_TOOL = $(host_toolchain)install_name_tool
 default_host_OTOOL = $(host_toolchain)otool
 default_host_NM = $(host_toolchain)nm
+default_host_OBJCOPY = $(host_toolchain)objcopy
+default_host_WINDRES = $(host_toolchain)windres
 
 define add_host_tool_func
 ifneq ($(filter $(origin $1),undefined default),)
@@ -35,5 +37,5 @@ host_$1 = $$($(host_arch)_$(host_os)_$1)
 host_$(release_type)_$1 = $$($(host_arch)_$(host_os)_$(release_type)_$1)
 endef
 
-$(foreach tool,CC CXX AR RANLIB STRIP NM LIBTOOL OTOOL INSTALL_NAME_TOOL,$(eval $(call add_host_tool_func,$(tool))))
+$(foreach tool,CC CXX AR RANLIB STRIP NM LIBTOOL OBJCOPY OTOOL INSTALL_NAME_TOOL WINDRES,$(eval $(call add_host_tool_func,$(tool))))
 $(foreach flags,CFLAGS CXXFLAGS CPPFLAGS LDFLAGS, $(eval $(call add_host_flags_func,$(flags))))
