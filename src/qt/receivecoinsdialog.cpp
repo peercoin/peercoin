9e54472c5b850fd2219fe9a4e6c6a2c8b551ae3e
backpacker69 <backpacker69@protonmail.com>
2024-07-25 14:21:48 +1000

    phase out P2SH_SEGWIT addresses, resolving #699

--- src/qt/receivecoinsdialog.cpp
+++ src/qt/receivecoinsdialog.cpp
@@ -94,7 +94,6 @@ void ReceiveCoinsDialog::setModel(WalletModel *_model)
             if (model->wallet().getDefaultAddressType() == type) ui->addressType->setCurrentIndex(index);
         };
         add_address_type(OutputType::LEGACY, tr("Base58 (Legacy)"), tr("Not recommended due to higher fees and less protection against typos."));
-        add_address_type(OutputType::P2SH_SEGWIT, tr("Base58 (P2SH-SegWit)"), tr("Generates an address compatible with older wallets."));
         add_address_type(OutputType::BECH32, tr("Bech32 (SegWit)"), tr("Generates a native segwit address (BIP-173). Some old wallets don't support it."));
         if (model->wallet().taprootEnabled()) {
             add_address_type(OutputType::BECH32M, tr("Bech32m (Taproot)"), tr("Bech32m (BIP-350) is an upgrade to Bech32, wallet support is still limited."));
