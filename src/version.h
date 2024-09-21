21f842be986f93ff2555301e3cb0b932bd2bb226
Marius Kjærstad <mkjaerstad@protonmail.com>
2024-09-21 23:51:09 +0200

    Disconnect from older peers

--- src/version.h
+++ src/version.h
@@ -16,7 +16,7 @@ static const int OLD_VERSION = 70014; // peercoin: used to communicate with clie
 static const int INIT_PROTO_VERSION = 209;
 
 //! disconnect from peers older than this proto version
-static const int MIN_PEER_PROTO_VERSION = 70016;
+static const int MIN_PEER_PROTO_VERSION = 70018;
 
 //! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
